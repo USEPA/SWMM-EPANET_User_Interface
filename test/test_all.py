c2d219e88f89ed5a7b61c061e8ae04d28947e86a
Paul.Duda <paul.duda@respec.com>
2016-02-02 14:03:18 -0500

    stub in unit tests for epanet options to string

--- test/test_all.py
+++ test/test_all.py
@@ -1,13 +1,25 @@
 import unittest
 from test_title import SimpleTitleTest
 from test_patterns import SimplePatternTest
+from test_curves import SimpleCurveTest
 
 
 runner = unittest.TextTestRunner()
 
 my_suite = unittest.TestSuite()
+
+# for MTP 1:
 my_suite.addTest(SimpleTitleTest())
+# my_suite.addTest(SimpleOptionsTest())
+# my_suite.addTest(SimpleReactionsTest())
+# my_suite.addTest(SimpleTimesTest())
+# my_suite.addTest(SimpleEnergyTest())
+# my_suite.addTest(SimpleReportTest())
+# my_suite.addTest(SimpleBackdropTest())
+
+# will need for later MTPs:
 my_suite.addTest(SimplePatternTest())
+# my_suite.addTest(SimpleCurveTest())
 
 runner.run(my_suite)
 
---
+++ test/test_curves.py
@@ -0,0 +1,22 @@
+from core.epanet import curves
+import unittest
+
+
+class SimpleCurveTest(unittest.TestCase):
+    def __init__(self):
+        unittest.TestCase.__init__(self)
+        self.my_curve = curves.Curve
+
+    def setUp(self):
+        self.my_curve = curves.Curve()
+        self.my_curve.curve_id = "XXX"
+        self.my_curve.description = "test curve"
+        self.my_curve.curve_type = curves.CurveType.HEAD_LOSS
+        self.my_curve.curve_xy = ((1500,250),(1400,200))
+
+    def runTest(self):
+        assert self.my_curve.curve_id == "XXX"
+        assert self.my_curve.description == "test curve"
+        xstr = self.my_curve.to_inp()
+
+        assert self.my_curve.to_inp() == 'XXX	1.0	1.1	1.2	1.3', 'incorrect pattern block'

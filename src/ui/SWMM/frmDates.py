ced7da440bbbdccc1dccd58af9fca038c6e4cf98
Paul.Duda <paul.duda@respec.com>
2016-02-10 13:55:37 -0500

    new forms for swmm options

--- src/ui/SWMM/frmDates.py
+++ src/ui/SWMM/frmDates.py
@@ -1,6 +1,6 @@
 import PyQt4.QtGui as QtGui
 import PyQt4.QtCore as QtCore
-import core.epanet.project
+import core.swmm.options.dates
 from ui.SWMM.frmDatesDesigner import Ui_frmDates
 
 
@@ -14,13 +14,29 @@ class frmDates(QtGui.QMainWindow, Ui_frmDates):
         self._parent = parent
 
     def set_from(self, project):
-        # section = core.epanet.project.Title()
-        section = project.find_section("TITLE")
-        self.txtTitle.setPlainText(str(section.title))
+        # section = core.swmm.options.dates.Dates
+        section = project.find_section("OPTIONS")
+        self.dedStart.setDate(section.start_date)
+        self.tmeStart.setTime(section.start_time)
+        self.dedStartReport.setDate(section.report_start_date)
+        self.tmeReport.setTime(section.report_start_time)
+        self.dedEnd.setDate(section.end_date)
+        self.tmeEnd.setTime(section.end_time)
+        self.dedSweepEnd.setdate(section.sweep_end)
+        self.dedSweepStart.setDate(section.sweep_start)
+        self.txtAntecedent.setText(section.dry_days)
 
     def cmdOK_Clicked(self):
-        section = self._parent.project.find_section("TITLE")
-        section.title = self.txtTitle.toPlainText()
+        section = self._parent.project.find_section("OPTIONS")
+        section.start_date = self.dedStart.text()
+        section.start_time = self.tmeStart.text()
+        section.report_start_date = self.dedStartReport.text()
+        section.report_start_time = self.tmeReport.text()
+        section.end_date = self.dedEnd.text()
+        section.end_time = self.tmeEnd.text()
+        section.sweep_end = self.dedSweepEnd.text()
+        section.sweep_start = self.dedSweepStart.text()
+        section.dry_days = self.txtAntecedent.text()
         self.close()
 
     def cmdCancel_Clicked(self):

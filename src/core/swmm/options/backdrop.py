9afd3e2128fcb679a9fe4d0a3f60b5cb3289f4e6
Paul.Duda <paul.duda@respec.com>
2016-02-23 13:44:43 -0500

    add set_text and get_text for swmm map and backdrop options

--- src/core/swmm/options/backdrop.py
+++ src/core/swmm/options/backdrop.py
@@ -1,4 +1,5 @@
-﻿from core.inputfile import Section
+﻿from core.inputfile import InputFile, Section
+from enum import Enum
 
 
 class BackdropOptions(Section):
@@ -14,3 +15,30 @@ class BackdropOptions(Section):
 
         self.file = "" 		                    # string
         """name of the file that contains the backdrop image"""
+
+    def get_text(self):
+        text_list = [BackdropOptions.SECTION_NAME]
+        if self.dimensions:
+            text_list.append(" {:17}\t{:16}\t{:16}\t{:16}\t{:16}".format("DIMENSIONS",
+                             self.dimensions[0], self.dimensions[1], self.dimensions[2], self.dimensions[3]))
+        if self.file:
+            text_list.append(" {:17}\t{}".format("FILE", self.file))
+        return '\n'.join(text_list)
+
+    def set_text(self, new_text):
+        BackdropOptions.__init__(self)
+        for line in new_text.splitlines():
+            try:
+                if line.startswith(';'):
+                    if self.comment:
+                        self.comment += '\n'
+                    self.comment += line
+                if not line.startswith('['):
+                    fields = line.split()
+                    if len(fields) > 1:
+                        if fields[0].lower() == "dimensions" and len(fields) > 4:
+                            self.dimensions = fields[1:5]
+                        else:
+                            self.setattr_keep_type(InputFile.printable_to_attribute(fields[0]), fields[1])
+            except:
+                print("BackdropOptions skipping input line: " + line)

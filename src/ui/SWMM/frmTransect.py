75a4182d1040ed3c4d7d6f15006d7ca834e21c0d
Paul.Duda <paul.duda@respec.com>
2016-03-29 15:42:04 -0400

    connecting SWMM transect editor

--- src/ui/SWMM/frmTransect.py
+++ src/ui/SWMM/frmTransect.py
@@ -10,16 +10,63 @@ class frmTransect(QtGui.QMainWindow, Ui_frmTransect):
         self.setupUi(self)
         QtCore.QObject.connect(self.cmdOK, QtCore.SIGNAL("clicked()"), self.cmdOK_Clicked)
         QtCore.QObject.connect(self.cmdCancel, QtCore.SIGNAL("clicked()"), self.cmdCancel_Clicked)
-        self.set_from(parent.project)
         self._parent = parent
+        self.transect_id = ''
+        # set for first transect for now
+        self.set_from(parent.project,'xx')
 
-    def set_from(self, project):
-        section = project.title
-        # self.txtTitle.setPlainText(project.title.title)
+    def set_from(self, project, transect_id):
+        # section = core.swmm.project.Transects
+        section = project.find_section("TRANSECTS")
+        transect_list = section.value[0:]
+        # assume we want to edit the first one
+        self.transect_id = transect_id
+        for transect in transect_list:
+            if transect.name: # == transect_id
+                # this is the transect we want to edit
+                self.txtName.setText(transect.name)
+                self.txtDescription.setText(transect.comment)
+                self.txtLeftBank.setText(transect.n_left)
+                self.txtRightBank.setText(transect.n_right)
+                self.txtChannel.setText(transect.n_channel)
+                self.txtLeftSta.setText(transect.overbank_left)
+                self.txtRightSta.setText(transect.overbank_right)
+                self.txtStations.setText(transect.stations_modifier)
+                self.txtElevations.setText(transect.elevations_modifier)
+                self.txtMeander.setText(transect.meander_modifier)
+                point_count = -1
+                for value in transect.stations:
+                    point_count += 1
+                    led = QtGui.QLineEdit(str(value[1]))
+                    self.tblTransect.setItem(point_count,0,QtGui.QTableWidgetItem(led.text()))
+                    led = QtGui.QLineEdit(str(value[0]))
+                    self.tblTransect.setItem(point_count,1,QtGui.QTableWidgetItem(led.text()))
 
     def cmdOK_Clicked(self):
-        section = self._parent.project.title
-        section.title = self.txtTitle.toPlainText()
+        section = self._parent.project.find_section("TRANSECTS")
+        transect_list = section.value[0:]
+        for transect in transect_list:
+            if transect.name == self.transect_id:
+                # this is the transect
+                transect.name = self.txtName.text()
+                transect.comment = self.txtDescription.text()
+                transect.n_left = self.txtLeftBank.text()
+                transect.n_right = self.txtRightBank.text()
+                transect.n_channel = self.txtChannel.text()
+                transect.elevations_modifier = self.txtElevations.text()
+                transect.meander_modifier = self.txtMeander.text()
+                transect.stations_modifier = self.txtStations.text()
+                transect.overbank_left = self.txtLeftSta.text()
+                transect.overbank_right = self.txtRightSta.text()
+                transect.stations = []
+                for row in range(self.tblTransect.rowCount()):
+                    if self.tblTransect.item(row,1):
+                        x = self.tblTransect.item(row,1).text()
+                        if len(x) > 0:
+                            if self.tblTransect.item(row,0):
+                                y = self.tblTransect.item(row,0).text()
+                                if len(y) > 0:
+                                    transect.stations.append((x,y))
         self.close()
 
     def cmdCancel_Clicked(self):

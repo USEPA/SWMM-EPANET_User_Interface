43323ca0de1de8a3d3e769cb44dff0f460d7d9cf
Xinyu Wei <(xwei@hgl.com)>
2016-05-06 18:27:51 -0400

    test project loop through all inp files in the Examples directory. Read, write, read, and compare the two project instances.

--- test/core/swmm/test_project.py
+++ test/core/swmm/test_project.py
@@ -3,16 +3,35 @@ import core.swmm.project
 import unittest
 import inspect
 
+
 class ProjectTest(unittest.TestCase):
     def __init__(self):
         unittest.TestCase.__init__(self)
         self.my_project = core.swmm.project.Project()
+        self.new_project = core.swmm.project.Project()
 
     def runTest(self):
         directory = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
-        for inp_filename in ["Examples/Example1.inp"]:
-            self.my_project.read_file(os.path.join(directory, inp_filename))
-            assert len(self.my_project.sections) == 43
+        # for inp_filename in ["Examples/Example1.inp"]:
+        example_path = os.path.join(directory,"Examples")
+        example_files = os.listdir(example_path)
+        for filename in example_files:
+            # self.my_project.read_file(os.path.join(directory, inp_filename))
+            file_extension = os.path.splitext(filename)[1]
+            if file_extension.lower() == ".inp":
+                self.my_project.read_file(filename)
+                new_filename = filename+"test"
+                self.my_project.write_file(new_filename)
+                self.new_project.read_file(new_filename)
+                number_of_sections = len(self.my_project.sections)
+                new_number_of_sections = len(self.new_project.sections)
+                assert number_of_sections == new_number_of_sections
+                if number_of_sections == new_number_of_sections:
+                    for (old_section,new_section) in \
+                            zip(self.my_project.sections, self.new_project.sections):
+                        pass
+                    # assert old_section == new_section
+            # assert len(self.my_project.sections) == 43
             pass
 
             # with open(inp_filename + ".written.txt", 'w') as writer:

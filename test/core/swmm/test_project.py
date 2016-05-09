5fed08227bb6c40add7387f81d427737ef0ca44a
Xinyu Wei <xwei@.hgl.com>
2016-05-09 17:30:28 -0400

    little update on test_project

--- test/core/swmm/test_project.py
+++ test/core/swmm/test_project.py
@@ -12,28 +12,37 @@ class ProjectTest(unittest.TestCase):
 
     def runTest(self):
         directory = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
-        # for inp_filename in ["Examples/Example1.inp"]:
-        example_path = os.path.join(directory,"Examples")
+        example_path = os.path.join(directory, "Examples")
         example_files = os.listdir(example_path)
+
+        # Loop through all .inp files in the Examples folder
         for filename in example_files:
-            # self.my_project.read_file(os.path.join(directory, inp_filename))
             file_extension = os.path.splitext(filename)[1]
             if file_extension.lower() == ".inp":
-                self.my_project.read_file(filename)
-                new_filename = filename+"test"
-                self.my_project.write_file(new_filename)
-                self.new_project.read_file(new_filename)
+                # Read .inp file, count the number of sections
+                my_file = os.path.join(example_path,filename)
+                self.my_project.read_file(my_file)
                 number_of_sections = len(self.my_project.sections)
+
+                # Writing my_project to new file .inptest
+                new_file = my_file + "test"
+                self.my_project.write_file(new_file)
+
+                # Read .inptest into new_project, count the number of sections, assert
+                self.new_project.read_file(new_file)
                 new_number_of_sections = len(self.new_project.sections)
                 assert number_of_sections == new_number_of_sections
+
+                # If the numbers of sections agree, match each section
                 if number_of_sections == new_number_of_sections:
-                    for (old_section,new_section) in \
+                    for (old_section, new_section) in \
                             zip(self.my_project.sections, self.new_project.sections):
+
                         pass
                     # assert old_section == new_section
                     # Sequences are different, how do we test these? Think about it now.
                     # How about run the SWMM and check the results ?
-                os.remove(new_filename)
+                # os.remove(new_filename)
             pass
 
             # assert len(self.my_project.sections) == 43

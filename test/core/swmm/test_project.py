71eb629be65185cae15989c7313e287eb227ff80
Xinyu Wei <(xwei@hgl.com)>
2016-05-11 21:56:54 -0400

    Make copies of .inp files using read_file and write_file, then execute all .inp files and the .inp_copy.

--- test/core/swmm/test_project.py
+++ test/core/swmm/test_project.py
@@ -2,7 +2,9 @@ import os
 import core.swmm.project
 import unittest
 import inspect
-from ui.swmm.frmRunSWMM import frmRunSWMM
+import shutil
+# from ui.swmm.frmRunSWMM import frmRunSWMM
+# from ui.model_utility import StatusMonitor0
 
 
 class ProjectTest(unittest.TestCase):
@@ -13,20 +15,31 @@ class ProjectTest(unittest.TestCase):
 
     def runTest(self):
         directory = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
+
+        # Get example path and file names
         example_path = os.path.join(directory, "Examples")
         example_files = os.listdir(example_path)
 
+        # Get path to swmm5.exe, copy the exe file to /Examples
+        source_root_path = os.path.split(os.path.split(os.path.split(directory)[0])[0])[0]
+        exe_path = os.path.join(os.path.join(source_root_path, "src"),"Externals")
+        exe_name = "swmm5.exe"
+        exe_full_path = os.path.join(exe_path, exe_name)
+        exe_to_example_path = os.path.join(example_path, exe_name)
+        shutil.copy(exe_full_path, exe_to_example_path)
+
         # Loop through all .inp files in the Examples folder
         for filename in example_files:
-            file_extension = os.path.splitext(filename)[1]
-            if file_extension.lower() == ".inp":
+            prefix, extension = os.path.splitext(filename)
+            if extension.lower() == ".inp":
                 # Read .inp file, count the number of sections
                 my_file = os.path.join(example_path,filename)
                 self.my_project.read_file(my_file)
                 number_of_sections = len(self.my_project.sections)
 
-                # Writing my_project to new file .inptest
-                new_file = my_file + "test"
+                # Write my_project to new file .inptest
+                new_filename = filename + "_copy"
+                new_file = os.path.join(example_path, new_filename)
                 self.my_project.write_file(new_file)
 
                 # Read .inptest into new_project, count the number of sections, assert
@@ -34,21 +47,51 @@ class ProjectTest(unittest.TestCase):
                 new_number_of_sections = len(self.new_project.sections)
                 assert number_of_sections == new_number_of_sections
 
-                # If the numbers of sections agree, match each section
+                # If the numbers of sections agree, run swmm5.exe using:
+                # swmm5.exe XXXXX1.inp XXXXXX1.rpt XXXXXX1.out
+                # Have trouble with path, copied swmm5.exe into the Examples folder
                 if number_of_sections == new_number_of_sections:
-                    for (old_section, new_section) in \
-                            zip(self.my_project.sections, self.new_project.sections):
 
-                        pass
+                    # Get current_directory for restoring it later
+                    current_directory = os.getcwd()
                     os.chdir(example_path)
-                    my_run = frmRunSWMM()
-                    my_run.RunSimulation()
-                    #my_run.DisplayRunStatus()
+                    # Run my_file
+                    inp_file = filename
+                    rpt_file = prefix + '.rpt'
+                    out_file = prefix + '.out'
+                    command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
+                    try:
+                        os.system(command_line)
+                    except:
+                        raise "Error in executing exes"
 
-                    # assert old_section == new_section
-                    # Sequences are different, how do we test these? Think about it now.
-                    # How about run the SWMM and check the results ?
-                # os.remove(new_filename)
+                    #Run new_file
+                    temp_file = "renameit.inp"
+                    os.rename(new_filename, temp_file)
+                    inp_file = temp_file
+                    rpt_file = prefix + '_copy'+ '.rpt'
+                    out_file = prefix + '_copy'+ '.out'
+                    command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
+                    try:
+                        os.system(command_line)
+                    except:
+                        raise "Error in executing exes"
+                    os.rename(temp_file, new_filename)
+                    # Return to current directory
+                    os.chdir(current_directory)
+                    # args = []
+                    # if os.path.isfile(exe_path):
+                    #     args.append(file_name)
+                    #     args.append(prefix + '.rpt')
+                    #     args.append(prefix + '.out')
+                    #    # running the Exe
+                    #    status = StatusMonitor0(exe_path, args, self, model='SWMM')
+                    #    status.show()
+                # for (old_section, new_section) in \
+                #         zip(self.my_project.sections, self.new_project.sections):
+                #    pass
+            # Sequences are different, how do we test these? Think about it now.
+            # How about run the SWMM and check the results ?
             pass
 
             # assert len(self.my_project.sections) == 43

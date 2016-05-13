883ba70a4eb3b7763d625dd481f039bcc732c430
Xinyu Wei <xwei@.hgl.com>
2016-05-13 12:11:24 -0400

    report Example test results in html

--- test/core/swmm/test_project.py
+++ test/core/swmm/test_project.py
@@ -4,6 +4,7 @@ import unittest
 import inspect
 import shutil
 import filecmp
+import webbrowser
 # from ui.swmm.frmRunSWMM import frmRunSWMM
 # from ui.model_utility import StatusMonitor0
 
@@ -123,109 +124,186 @@ class ProjectTest(unittest.TestCase):
         # Loop through all .inp files in /Examples and first-level sub directories
         # Record results in diff_summaries.txt under /Examples
         # Need to use assert -- Thinking
-        diff_file = os.path.join(example_root_path, "diff_summaries.txt")
-        with open(diff_file, "w") as text_file:
-            text_file.write('Loop through all .inp files: \n')
-            for example_path in example_paths:
+        # diff_file = os.path.join(example_root_path, "diff_summaries.txt")
+        examples = [] # example name
+        status = []   # example test status 'Pass' or 'Fail'
+        remarks = []  # error message goes here
+        html_file = os.path.join(directory, 'Test_Example_Summaries.html')
 
-                text_file.write(example_path+'\n')
-                # Get all file names
-                example_files = os.listdir(example_path)
+        # Open html file
+        text_file = open(html_file, "w")
+        text_file.write('<header><h1>'+'EXAMPLES TEST REPORT:'+'</h1></header>')
 
-                # Test all examples in the /Examples folder
-                # Copy the exe file to /Examples or sub directory
-                exe_to_example_path = os.path.join(example_path, exe_name)
-                shutil.copy(exe_full_path, exe_to_example_path)
+        for example_path in example_paths:
 
-                # Loop through all .inp files in the /Examples folder
-                for filename in example_files:
-                    prefix, extension = os.path.splitext(filename)
-                    if extension.lower() == ".inp":
+            # Get all file names
+            example_files = os.listdir(example_path)
 
-                        # Read .inp file, count the number of sections
-                        my_file = os.path.join(example_path, filename)
-                        self.my_project.read_file(my_file)
-                        number_of_sections = len(self.my_project.sections)
+            # Test all examples in the /Examples folder
+            # Copy the exe file to /Examples or sub directory
+            exe_to_example_path = os.path.join(example_path, exe_name)
+            shutil.copy(exe_full_path, exe_to_example_path)
 
-                        # Write my_project to new file .inptest
-                        new_filename = filename + "_copy"
-                        new_file = os.path.join(example_path, new_filename)
-                        self.my_project.write_file(new_file)
+            # Loop through all .inp files in the /Examples folder
+            for filename in example_files:
+                prefix, extension = os.path.splitext(filename)
+                if extension.lower() == ".inp":
 
-                        # Read .inptest into new_project, count the number of sections, assert
-                        self.new_project.read_file(new_file)
-                        new_number_of_sections = len(self.new_project.sections)
-                        assert number_of_sections == new_number_of_sections
+                    # Read .inp file, count the number of sections
+                    my_file = os.path.join(example_path, filename)
+                    self.my_project.read_file(my_file)
+                    number_of_sections = len(self.my_project.sections)
 
-                        # If the numbers of sections agree, run swmm5.exe using:
-                        # swmm5.exe XXXXX1.inp XXXXXX1.rpt XXXXXX1.out
-                        # Have trouble with path, copied swmm5.exe into the Examples folder
-                        if number_of_sections == new_number_of_sections:
+                    # Write my_project to new file .inptest
+                    new_filename = filename + "_copy"
+                    new_file = os.path.join(example_path, new_filename)
+                    self.my_project.write_file(new_file)
 
-                            # Get current_directory for restoring it later
-                            current_directory = os.getcwd() #current test/core/
-                            os.chdir(example_path)
-                            # Run my_file
-                            inp_file = filename
-                            rpt_file = prefix + '.rpt'
-                            out_file = prefix + '.out'
-                            command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
-                            try:
-                                os.system(command_line)
-                            except:
-                                raise "Error in executing{}".format(command_line)
+                    # Read .inptest into new_project, count the number of sections, assert
+                    self.new_project.read_file(new_file)
+                    new_number_of_sections = len(self.new_project.sections)
+                    assert number_of_sections == new_number_of_sections
 
-                            # Run new_file (copy of original .inp)
-                            # Must rename the .inp_copy to renameit.inp
-                            # swmm5.exe requires .inp as input files
-                            # but the duplicates should not be named as .inp
-                            # They will be renamed to .inp_copy after the run
-                            temp_file = "renameit.inp"
-                            try:
-                                os.remove(temp_file)
-                            except OSError:
-                                pass
-                            os.rename(new_filename, temp_file)
-                            inp_file = temp_file
-                            rpt_file = prefix + '_copy'+ '.rpt'
-                            out_file = prefix + '_copy'+ '.out'
-                            command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
-                            try:
-                                os.system(command_line)
-                            except:
-                                raise "Error in executing{}".format(command_line)
+                    # If the numbers of sections agree, run swmm5.exe using:
+                    # swmm5.exe XXXXX1.inp XXXXXX1.rpt XXXXXX1.out
+                    # Have trouble with path, copied swmm5.exe into the Examples folder
+                    if number_of_sections == new_number_of_sections:
 
-                            # Compare two out files
-                            original_ = os.path.join(example_path, prefix + '.out')
-                            copy_ = os.path.join(example_path, prefix + '_copy'+ '.out')
-                            if os.path.isfile(original_) and os.path.isfile(copy_):
-                                same_ = filecmp.cmp(original_,copy_)
-                                if not same_:
-                                    text_file.write(prefix+'.out binary differ\n')
-                                else:
-                                    text_file.write(prefix + '.out binary same\n')
+                        # Get current_directory for restoring it later
+                        current_directory = os.getcwd() #current test/core/
+                        os.chdir(example_path)
+                        # Run my_file
+                        inp_file = filename
+                        rpt_file = prefix + '.rpt'
+                        out_file = prefix + '.out'
+                        command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
+                        try:
+                            os.system(command_line)
+                        except:
+                            msg = "Error in executing{}".format(command_line)
+                            examples.append(my_file)
+                            status.append('----')
+                            remarks.append(msg)
+                            pass
+
+                        # Run new_file (copy of original .inp)
+                        # Must rename the .inp_copy to renameit.inp
+                        # swmm5.exe requires .inp as input files
+                        # but the duplicates should not be named as .inp
+                        # They will be renamed to .inp_copy after the run
+                        temp_file = "renameit.inp"
+                        try:
+                            os.remove(temp_file)
+                        except OSError:
+                            pass
+                        os.rename(new_filename, temp_file)
+                        inp_file = temp_file
+                        rpt_file = prefix + '_copy'+ '.rpt'
+                        out_file = prefix + '_copy'+ '.out'
+                        command_line = exe_name + " " + inp_file + " " + rpt_file + " "+out_file
+                        try:
+                            os.system(command_line)
+                        except:
+                            msg = "Error in executing{}".format(command_line)
+                            examples.append(new_file)
+                            status.append('----')
+                            remarks.append(msg)
+                            pass
+
+                        # Compare two .out files
+                        original_ = os.path.join(example_path, prefix + '.out')
+                        copy_ = os.path.join(example_path, prefix + '_copy'+ '.out')
+
+                        # If both exists
+                        if os.path.isfile(original_) and os.path.isfile(copy_):
+                            same_ = filecmp.cmp(original_,copy_)
+                            if not same_:
+                                msg = prefix+'.out binary differ, ' \
+                                             'results of modified differ from results of original\n'
+                                examples.append(original_)
+                                status.append('Fail')
+                                remarks.append(msg)
                             else:
-                                #.out may not be produced if model fails
-                                text_file.write(prefix + '.out does not exist\n')
+                                msg = prefix + '.out successful matching\n'
+                                examples.append(original_)
+                                status.append('Pass')
+                                remarks.append(msg)
 
-                            # Compare two rpt files
-                            exempted_strings = ["Analysis begun on", "Analysis ended on",
-                                                "Total elapsed time", "ERROR"]
-                            original_ = os.path.join(example_path, prefix + '.rpt')
-                            copy_ = os.path.join(example_path, prefix + '_copy'+ '.rpt')
-                            if os.path.isfile(original_) and os.path.isfile(copy_):
-                                diff_msg = ProjectTest.compare_two_files(original_, copy_, exempted_strings)
-                                if diff_msg:
-                                    text_file.write(prefix + '.rpt:'+'\n')
-                                    text_file.write(diff_msg + '\n')
-                                else:
-                                    text_file.write(prefix + '.rpt same text none white' + '\n')
+                        # If the original does not produce .out '----'
+                        elif not os.path.isfile(original_):
+                            msg = prefix + '.out does not exist, ' \
+                                           'original input did not run\n'
+                            examples.append(original_)
+                            status.append('----')
+                            remarks.append(msg)
+
+                        # If the original does but the copy does not produce .out 'Fail'
+                        else:
+                            msg = prefix + '_copy.out does not exist, ' \
+                                           'original ran but modified did not \n'
+                            examples.append(original_)
+                            status.append('Fail')
+                            remarks.append(msg)
+
+                        # Compare two rpt files
+                        exempted_strings = ["Analysis begun on", "Analysis ended on",
+                                            "Total elapsed time", "ERROR"]
+                        original_ = os.path.join(example_path, prefix + '.rpt')
+                        copy_ = os.path.join(example_path, prefix + '_copy'+ '.rpt')
+                        if os.path.isfile(original_) and os.path.isfile(copy_):
+                            diff_msg = ProjectTest.compare_two_files(original_, copy_, exempted_strings)
+                            if diff_msg:
+                                msg = prefix + '.rpt:'\
+                                      +'results of modified differ from results of original'+'\n'\
+                                      + diff_msg + '\n'
+                                examples.append(original_)
+                                status.append('Fail')
+                                remarks.append(msg)
                             else:
-                                text_file.write(prefix + '.rpt does not exist' + '\n')
+                                msg = prefix + '.rpt successful matching' + '\n'
+                                examples.append(original_)
+                                status.append('Pass')
+                                remarks.append(msg)
 
-                            # Rename file to .inp_test because use .inp only for originals
-                            os.rename(temp_file, new_filename)
+                        # If the original does not produce .out '----'
+                        elif not os.path.isfile(original_):
+                            msg = prefix + '.rpt does not exist, ' \
+                                           'original input did not run\n'
+                            examples.append(original_)
+                            status.append('----')
+                            remarks.append(msg)
 
-                            # Return to current directory
-                            os.chdir(current_directory)
+                        # If the original does but the copy does not produce .out 'Fail'
+                        else:
+                            msg = prefix + '_copy.rpt does not exist,' \
+                                           'original ran but modified did not\n'
+                            examples.append(original_)
+                            status.append('Fail')
+                            remarks.append(msg)
 
+                        # Rename file to .inp_test because use .inp only for originals
+                        os.rename(temp_file, new_filename)
+
+                        # Return to current directory
+                        os.chdir(current_directory)
+
+        # Write HTML table
+        # Print opening HTML tags -------------------------
+        text_file.write("<htm><body><table border='1'>")
+        # Print the content of the table, line by line ----
+        for i in range(0, len(examples)):
+            if status[i] == 'Fail':
+                text_file.write("<tr><td>" + examples[i] + "</td><td>"
+                        + '<font color="red">' + status[i] + '</font>' + "</td><td>"
+                        + remarks[i] + "</td></tr>")
+            else:
+                text_file.write("<tr><td>" + examples[i] + "</td><td>"
+                        + status[i] + "</td><td>"
+                        + remarks[i] + "</td></tr>")
+        # Print closing HTML tags -------------------------
+        text_file.write("</table></body></html>")
+        text_file.close()
+        try:
+            webbrowser.open_new_tab('file://' + html_file)
+        except:
+            print("Error writing test results to " + html_file)

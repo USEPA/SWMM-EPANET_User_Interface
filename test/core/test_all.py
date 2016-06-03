2405b18fcb7763fa50a03b154e062e4fe551dfaa
Xinyu Wei <(xwei@hgl.com)>
2016-06-03 11:22:00 -0400

    added coverage to test_all.py.

--- test/core/test_all.py
+++ test/core/test_all.py
@@ -1,5 +1,7 @@
+import os
 import webbrowser
 import unittest
+import coverage
 import test.HTMLTestRunner
 import test.core.epanet.test_all
 import test.core.swmm.test_all
@@ -17,9 +19,33 @@ if __name__ == "__main__":
         title='SWMM-EPANET Core Test Report',
         description='Unit test results')
 
+    # Create a coverage instance
+    cov = coverage.Coverage()
+    cov.start()
+
     runner.run(my_suite)
+
+    cov.stop()
+    cov.save()
+    cov.html_report()
+
     fp.close()
+
+    # Open coverage report
+    current_path = os.getcwd()
+    full_file_path = os.path.join(current_path, 'htmlcov')
+    full_file_name = os.path.join(full_file_path, 'index.html')
+    try:
+        webbrowser.open_new_tab(full_file_name)
+    except:
+        print("Error opening coverage results")
+
+    # Open unit_test reports
     try:
         webbrowser.open_new_tab(report_filename)
     except:
         print("Test results written to " + report_filename)
+
+
+
+

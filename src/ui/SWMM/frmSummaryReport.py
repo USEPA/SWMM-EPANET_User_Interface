dde068a332d3324948b15b24e7fb7f36dfec93f3
Paul.Duda <paul.duda@respec.com>
2016-06-08 17:42:57 -0400

    SWMM summary report

--- src/ui/SWMM/frmSummaryReport.py
+++ src/ui/SWMM/frmSummaryReport.py
@@ -64,7 +64,7 @@ class frmSummaryReport(QtGui.QMainWindow, Ui_frmSummaryReport):
                             found_start = True
                         elif found_start:
                             if reading_real:
-                                if len(line.strip()) == 0:
+                                if len(line.strip()) == 0 or line.startswith('  -------'):
                                     reading_real = False
                                     found_start = False
                                 else:
@@ -79,88 +79,152 @@ class frmSummaryReport(QtGui.QMainWindow, Ui_frmSummaryReport):
                                     headers.append(line)
                             elif reading_headers == False and line.startswith('  -------'):
                                 reading_headers = True
-                    self.tblSummary.setVerticalHeaderLabels(row_labels)
                     nrows = len(real_rows)
                     ncols = len(real_rows[0]) - 1
                     self.tblSummary.setRowCount(nrows)
                     self.tblSummary.setColumnCount(ncols)
+                    self.tblSummary.setVerticalHeaderLabels(row_labels)
 
                     column_headers = []
-                    if self.cboType.currentText() == 'Subcatchment Runoff':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        Units3 = ' CFS'
-                        headers = ['Precip' + Units1,'Runon' + Units1,'Total Evap' + Units1,'Total Infil' + Units1,
-                                  'Total Runoff' + Units1,'Total Runoff' + Units2,'Peak Runoff' + Units3,'Runoff Coeff']
-                    elif self.cboType.currentText() == 'LID Performance':
-                        Units1 = ' in'
-                        headers = ['LID Control','Total Inflow' + Units1,'Evap Loss' + Units1,'Infil Loss' + Units1,
-                                   'Surface Outflow' + Units1,'Drain Outflow' + Units1,'Initial Storage' + Units1,
-                                   'Final Storage' + Units1,'Continuity Error %']
+                    total_num_headers = len(headers)
+                    header_list = headers[total_num_headers-1].split()
+                    if self.cboType.currentText() == 'LID Performance':
+                        Units1 = header_list[3]
+                        column_headers = ['LID Control',
+                                          'Total Inflow' + '\n' + Units1,
+                                          'Evap Loss' + '\n' + Units1,
+                                          'Infil Loss' + '\n' + Units1,
+                                          'Surface Outflow' + '\n' + Units1,
+                                          'Drain Outflow' + '\n' + Units1,
+                                          'Initial Storage' + '\n' + Units1,
+                                          'Final Storage' + '\n' + Units1,
+                                          'Continuity Error %']
                     elif self.cboType.currentText() == 'Groundwater':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        Units3 = ' CFS'
-                        headers = ['Total Infil' + Units1,'Total Evap' + Units1,'Total Lower Seepage' + Units1,
-                                   'Total Lateral Outflow' + Units1,'Maximum Lateral Outflow' + Units2,'Average Upper Moisture',
-                                  'Average Water Table' + Units3,'Final Upper Moisture','Final Water Table' + Units3]
-                    elif self.cboType.currentText() == 'Subcatchment Washoff':
-                        Units1 = ' in'
-                        headers = ['Pollutant 1' + Units1,'Pollutant 2' + Units1]
+                        Units1 = header_list[3]
+                        Units2 = header_list[5] + ' ' + header_list[6]
+                        Units3 = header_list[10]
+                        column_headers = ['Total' + '\n' + 'Infil' + '\n' + Units1,
+                                          'Total' + '\n' + 'Evap' + '\n' + Units1,
+                                          'Total' + '\n' + 'Lower' + '\n' + 'Seepage' + '\n' + Units1,
+                                          'Total' + '\n' + 'Lateral' + '\n' + 'Outflow' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'Lateral' + '\n' + 'Outflow' + '\n' + Units2,
+                                          'Average' + '\n' + 'Upper' + '\n' + 'Moisture',
+                                          'Average' + '\n' + 'Water' + '\n' + 'Table' + '\n' + Units3,
+                                          'Final' + '\n' + 'Upper' + '\n' + 'Moisture',
+                                          'Final' + '\n' + 'Water' + '\n' + 'Table' + '\n' + Units3]
                     elif self.cboType.currentText() == 'Node Depth':
-                        Units1 = ' in'
-                        headers = ['Type','Average Depth' + Units1,'Maximum Depth' + Units1,'Maximum HGL' + Units1,
-                                   'Day of Maximum Depth','Hour of Maximum Depth','Maximum Reported Depth' + Units1]
+                        Units1 = header_list[3]
+                        column_headers = ['Type',
+                                          'Average' + '\n' + 'Depth' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'Depth' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'HGL' + '\n' + Units1,
+                                          'Day of' + '\n' + 'Maximum' + '\n' + ' Depth',
+                                          'Hour of' + '\n' + 'Maximum' + '\n' + 'Depth',
+                                          'Maximum' + '\n' + 'Reported' + '\n' + 'Depth' + '\n' + Units1]
                     elif self.cboType.currentText() == 'Node Inflow':
-                        Units1 = ' in'
-                        Units2 = ' in'
-                        headers = ['Type','Maximum Lateral Inflow' + Units1,'Maximum Total Inflow' + Units1,'Day of Maximum Inflow',
-                                   'Hour of Maximum Inflow','Lateral Inflow Volume' + Units2,'Total Inflow Volume' + Units2,
-                                   'Flow Balance Error Percent']
+                        Units1 = header_list[3]
+                        Units2 = header_list[6] + ' ' + header_list[7]
+                        column_headers = ['Type',
+                                          'Maximum' + '\n' + 'Lateral' + '\n' + 'Inflow' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'Total' + '\n' + 'Inflow' + '\n' + Units1,
+                                          'Day of' + '\n' + 'Maximum' + '\n' + 'Inflow',
+                                          'Hour of' + '\n' + 'Maximum' + '\n' + 'Inflow',
+                                          'Lateral' + '\n' + 'Inflow' + '\n' + 'Volume' + '\n' + Units2,
+                                          'Total' + '\n' + 'Inflow' + '\n' + 'Volume' + '\n' + Units2,
+                                          'Flow' + '\n' + 'Balance' + '\n' + 'Error' + '\n' + 'Percent']
                     elif self.cboType.currentText() == 'Node Surcharge':
-                        Units1 = ' in'
-                        headers = ['Type','Hours Surcharged','Max Height Above Crown' + Units1,'Min Depth Below Rim' + Units1]
+                        Units1 = header_list[3]
+                        column_headers = ['Type',
+                                          'Hours' + '\n' + 'Surcharged',
+                                          'Max Height' + '\n' + 'Above' + '\n' + 'Crown' + '\n' + Units1,
+                                          'Min Depth' + '\n' + 'Below' + '\n' + 'Rim' + '\n' + Units1]
                     elif self.cboType.currentText() == 'Node Flooding':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        Units3 = ' CFS'
-                        headers = ['Hours Flooded','Maximum Rate' + Units1,'Day of Maximum Flooding','Hour of Maximum Flooding',
-                                   'Total Flood Volume' + Units2,'Maximum Ponded Volume' + Units3]
+                        Units1 = header_list[2]
+                        Units2 = header_list[5] + ' ' + header_list[6]
+                        Units3 = header_list[7] + ' ' + header_list[8]
+                        column_headers = ['Hours' + '\n' + 'Flooded',
+                                          'Maximum' + '\n' + 'Rate' + Units1,
+                                          'Day of' + '\n' + 'Maximum' + '\n' + 'Flooding',
+                                          'Hour of' + '\n' + 'Maximum' + '\n' + 'Flooding',
+                                          'Total' + '\n' + 'Flood' + '\n' + 'Volume' + '\n' + Units2,
+                                          'Maximum' + '\n' + 'Ponded' + '\n' + 'Volume' + '\n' + Units3]
                     elif self.cboType.currentText() == 'Storage Volume':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        headers = ['Average Volume' + Units1,'Average Percent Full','Evap Percent Loss','Exfil Percent Loss',
-                                   'Maximum Volume' +Units1,'Maximum Percent Full','Day of Maximum Volume','Hour of Maximum Volume',
-                                   'Maximum Outflow' +Units2]
-                    elif self.cboType.currentText() == 'Outfall Loading':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        Units3 = ' CFS'
-                        headers = ['Flow Freq. Pcnt.','Avg. Flow' + Units1,'Max. Flow' + Units1,'Total Volume' + Units2,
-                                   'Pollutant 1' + Units3,'Pollutant 2' + Units3]
+                        Units1 = header_list[1]
+                        Units2 = header_list[9] + ' ' + header_list[10]
+                        column_headers = ['Average' + '\n' + 'Volume' + '\n' + Units1,
+                                          'Average' + '\n' + 'Percent' + '\n' + 'Full',
+                                          'Evap' + '\n' + 'Percent' + '\n' + 'Loss',
+                                          'Exfil' + '\n' + 'Percent' + '\n' + 'Loss',
+                                          'Maximum' + '\n' + 'Volume' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'Percent' + '\n' + 'Full',
+                                          'Day of' + '\n' + 'Maximum' + '\n' + 'Volume',
+                                          'Hour of' + '\n' + 'Maximum' + '\n' + 'Volume',
+                                          'Maximum' + '\n' + 'Outflow' + '\n' + Units2]
                     elif self.cboType.currentText() == 'Link Flow':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        headers = ['Type','Maximum |Flow|' + Units1,'Day of Maximum Flow','Hour of Maximum Flow',
-                                   'Maximum |Velocity|' + Units2,'Max / Full Flow','Max / Full Depth']
+                        Units1 = header_list[2]
+                        Units2 = header_list[5]
+                        column_headers = ['Type',
+                                          'Maximum' + '\n' + '|Flow|' + '\n' + Units1,
+                                          'Day of' + '\n' + 'Maximum' + '\n' + 'Flow',
+                                          'Hour of' + '\n' + 'Maximum' + '\n' + 'Flow',
+                                          'Maximum' + '\n' + '|Velocity|' + '\n' + Units2,
+                                          'Max /' + '\n' + 'Full' + '\n' + 'Flow',
+                                          'Max /' + '\n' + 'Full' + '\n' + 'Depth']
                     elif self.cboType.currentText() == 'Flow Classification':
-                        headers = ['Adjusted/ Actual Length','Fully Dry','Upstrm Dry','Dnstrm Dry','Sub Critical','Super Critical',
-                                   'Upstrm Critical','Dnstrm Critical','Normal Flow Limited','Inlet Control']
+                        column_headers = ['Adjusted/' + '\n' + 'Actual' + '\n' + 'Length',
+                                          'Fully' + '\n' + 'Dry',
+                                          'Upstrm' + '\n' + 'Dry',
+                                          'Dnstrm' + '\n' + 'Dry',
+                                          'Sub' + '\n' + 'Critical',
+                                          'Super' + '\n' + 'Critical',
+                                          'Upstrm' + '\n' + 'Critical',
+                                          'Dnstrm' + '\n' + 'Critical',
+                                          'Normal' + '\n' + 'Flow' + '\n' + 'Limited',
+                                          'Inlet' + '\n' + 'Control']
                     elif self.cboType.currentText() == 'Conduit Surcharge':
-                        headers = ['Hours Both Ends Full','Hours Upstream Full','Hours Dnstream Full','Hours Above Normal Flow',
-                                   'Hours Capacity Limited']
+                        column_headers = ['Hours' + '\n' + 'Both Ends' + '\n' + 'Full',
+                                          'Hours' + '\n' + 'Upstream' + '\n' + 'Full',
+                                          'Hours' + '\n' + 'Dnstream' + '\n' + 'Full',
+                                          'Hours' + '\n' + 'Above' + '\n' + 'Normal' + '\n' + 'Flow',
+                                          'Hours' + '\n' + 'Capacity' + '\n' + 'Limited']
                     elif self.cboType.currentText() == 'Pumping':
-                        Units1 = ' in'
-                        Units2 = ' 10^6 gal'
-                        Units3 = ' CFS'
-                        headers = ['Percent Utilized','Number of Start-Ups','Minimum Flow' + Units1,'Average Flow' + Units1,
-                                   'Maximum Flow' + Units1,'Total Volume' + Units2,'Power Usage' + Units3,
-                                   '% Time Below Pump Curve','% Time Above Pump Curve']
-                    elif self.cboType.currentText() == 'Link Pollutant Load':
-                        Units1 = ' in'
-                        headers = ['Pollutant 1' + Units1,'Pollutant 2' + Units1]
-                    self.tblSummary.setHorizontalHeaderLabels(headers)
+                        Units1 = header_list[3]
+                        Units2 = header_list[6] + header_list[7]
+                        Units3 = header_list[8]
+                        column_headers = ['Percent' + '\n' + 'Utilized',
+                                          'Number of' + '\n' + 'Start-Ups',
+                                          'Minimum' + '\n' + 'Flow' + '\n' + Units1,
+                                          'Average' + '\n' + 'Flow' + '\n' + Units1,
+                                          'Maximum' + '\n' + 'Flow' + '\n' + Units1,
+                                          'Total' + '\n' + 'Volume' + '\n' + Units2,
+                                          'Power' + '\n' + 'Usage' + '\n' + Units3,
+                                          '% Time' + '\n' + 'Below' + '\n' + 'Pump' + '\n' + 'Curve',
+                                          '% Time' + '\n' + 'Above' + '\n' + 'Pump' + '\n' + 'Curve']
+                    else:
+                        # more generically get from headers
+                        header_list = headers[0].split()
+                        total_num_headers = len(headers)
+                        header_count = 1
+                        for header_row in headers[1:]:
+                            header_count += 1
+                            temp_header_list = header_row.split()
+                            num_column = len(header_list)
+                            for i in range(0,num_column):
+                                if header_count == total_num_headers:
+                                    if temp_header_list[1] == 'Node':
+                                        temp_header_list.remove('Node')
+                                    if temp_header_list[i+1] == '10^6' and temp_header_list[i+2] == 'gal':
+                                        temp_header_list[i+1] = '10^6 gal'
+                                        temp_header_list[i+2] = temp_header_list[i+3]
+                                        if len(temp_header_list) > i+4:
+                                            temp_header_list[i+3] = temp_header_list[i+4]
+                                    header_list[i] += '\n' + temp_header_list[i+1]
+                                else:
+                                    header_list[i] += '\n' + temp_header_list[i]
+                        column_headers = header_list
 
+                    # now populate grid
+                    self.tblSummary.setHorizontalHeaderLabels(column_headers)
                     row = -1
                     for line_list in real_rows:
                         col = -1
@@ -169,6 +233,7 @@ class frmSummaryReport(QtGui.QMainWindow, Ui_frmSummaryReport):
                             col += 1
                             item = QtGui.QTableWidgetItem(value)
                             item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+                            item.setTextAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignRight)
                             self.tblSummary.setItem(row,col,item)
             except Exception as e:
                 print("Error reading " + self.status_file_name)

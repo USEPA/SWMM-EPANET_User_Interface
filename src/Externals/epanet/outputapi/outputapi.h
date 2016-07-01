ccc95f0a940278ef48c787143f6e142eb7357ec9
Michael Tryby <tryby.michael@epa.gov>
2016-07-01 16:07:56 -0400

    Added functions for accessing energy and reaction reports. Refactored code to fix element indexing errors. Improved binary file validation and error checking. Combined functions for getting node and link IDs into a single function.

--- src/Externals/epanet/outputapi/outputapi.h
+++ src/Externals/epanet/outputapi/outputapi.h
@@ -7,20 +7,13 @@
 
 #ifndef OUTPUTAPI_H_
 #define OUTPUTAPI_H_
-
-#define   MAXFNAME  259      /* Max. # characters in file name         */
-#define   MAXTITLE  3        /* Max. # title lines                     */
-
-#define MAXID        31       // Max. # characters in ID name
-#define MAXMSG       79       // Max. # characters in message text
-#define NNODERESULTS  4       // number of result fields for nodes
-#define NLINKRESULTS  8       // number of result fields for links
-
-
-
-
 /* Epanet Results binary file API */
-typedef struct ENResultsAPI ENResultsAPI; // opaque struct object
+
+#define MAXFNAME     259   // Max characters in file name
+#define MAXID         31   // Max characters in ID name
+
+// This is an opaque struct. Do not access variables.
+typedef struct ENResultsAPI ENResultsAPI;
 
 typedef enum {
 	ENR_node = 1,
@@ -30,7 +23,9 @@ typedef enum {
 typedef enum {
     ENR_getSeries    = 1,
     ENR_getAttribute = 2,
-    ENR_getResult    = 3
+    ENR_getResult    = 3,
+	ENR_getReacts    = 4,
+	ENR_getEnergy    = 5
 } ENR_ApiFunction;
 
 typedef enum {
@@ -54,22 +49,27 @@ typedef enum {
 }ENR_Time;
 
 typedef enum {
-    ENR_demand   = 0,
-    ENR_head     = 1,
-    ENR_pressure = 2,
-    ENR_quality  = 3
+    ENR_demand   = 1,
+    ENR_head     = 2,
+    ENR_pressure = 3,
+    ENR_quality  = 4
 } ENR_NodeAttribute;
 
 typedef enum {
-    ENR_flow         = 0,
-    ENR_velocity     = 1,
-    ENR_headloss     = 2,
-    ENR_avgQuality   = 3,
-    ENR_status       = 4,
-    ENR_setting      = 5,
-    ENR_rxRate       = 6,
-    ENR_frctnFctr    = 7
+    ENR_flow         = 1,
+    ENR_velocity     = 2,
+    ENR_headloss     = 3,
+    ENR_avgQuality   = 4,
+    ENR_status       = 5,
+    ENR_setting      = 6,
+    ENR_rxRate       = 7,
+    ENR_frctnFctr    = 8
 } ENR_LinkAttribute;
+
+//typedef enum {
+//	ENR_power    = 0,
+//	ENR_reaction = 1
+//} ENR_SummaryType;
 
 
 #ifdef WINDOWS
@@ -87,46 +87,49 @@ typedef enum {
 #endif
 
 
+ENResultsAPI* DLLEXPORT ENR_init(void);
 
-int DLLEXPORT ENR_open(ENResultsAPI* *enrapi, const char* path);
+int DLLEXPORT ENR_open(ENResultsAPI* enrapi, const char* path);
 
-int DLLEXPORT ENR_getEnVersion(ENResultsAPI* enrapi, int* version);
-int DLLEXPORT ENR_getWarningCode(ENResultsAPI* enrapi, int* warncode);
+int DLLEXPORT ENR_getVersion(ENResultsAPI* enrapi, int* version);
 
 int DLLEXPORT ENR_getNetSize(ENResultsAPI* enrapi, ENR_ElementCount code, int* count);
+
 int DLLEXPORT ENR_getUnits(ENResultsAPI* enrapi, ENR_Unit code, int* unitFlag);
+
 int DLLEXPORT ENR_getTimes(ENResultsAPI* enrapi, ENR_Time code, int* time);
 
+int DLLEXPORT ENR_getElementName(ENResultsAPI* enrapi, ENR_ElementType type,
+		int elementIndex, char* name);
 
-int DLLEXPORT ENR_getNodeValue(ENResultsAPI* enrapi, int timeIndex, int nodeIndex, ENR_NodeAttribute attr, float *value);
-int DLLEXPORT ENR_getLinkValue(ENResultsAPI* enrapi, int timeIndex, int linkIndex, ENR_LinkAttribute attr, float *value);
+//int DLLEXPORT ENR_getNodeID(ENResultsAPI* enrapi, int nodeIndex, char* id);
 
-int DLLEXPORT ENR_getNodeID(ENResultsAPI* enrapi, int nodeIndex, char* id);
-int DLLEXPORT ENR_getLinkID(ENResultsAPI* enrapi, int linkIndex, char* id);
+//int DLLEXPORT ENR_getLinkID(ENResultsAPI* enrapi, int linkIndex, char* id);
 
+int DLLEXPORT ENR_getEnergyUsage(ENResultsAPI* enrapi, int pumpIndex,
+		int* linkIndex, float* values);
+int DLLEXPORT ENR_getNetReacts(ENResultsAPI* enrapi, float* values);
 
-float* ENR_newOutValueSeries(ENResultsAPI* enrapi, int seriesStart,
-        int seriesLength, int* length, int* errcode);
-float* ENR_newOutValueArray(ENResultsAPI* enrapi, ENR_ApiFunction func,
+float* DLLEXPORT ENR_newOutValueSeries(ENResultsAPI* enrapi, int startPeriod,
+        int endPeriod, int* length, int* errcode);
+float* DLLEXPORT ENR_newOutValueArray(ENResultsAPI* enrapi, ENR_ApiFunction func,
         ENR_ElementType type, int* length, int* errcode);
-
 int DLLEXPORT ENR_getNodeSeries(ENResultsAPI* enrapi, int nodeIndex, ENR_NodeAttribute attr,
-        int timeIndex, int length, float* outValueSeries);
+        int startPeriod, int length, float* outValueSeries);
 int DLLEXPORT ENR_getLinkSeries(ENResultsAPI* enrapi, int linkIndex, ENR_LinkAttribute attr,
-        int timeIndex, int length, float* outValueSeries);
-
-int DLLEXPORT ENR_getNodeAttribute(ENResultsAPI* enrapi, int timeIndex,
+        int startPeriod, int length, float* outValueSeries);
+int DLLEXPORT ENR_getNodeAttribute(ENResultsAPI* enrapi, int periodIndex,
         ENR_NodeAttribute attr, float* outValueArray);
-int DLLEXPORT ENR_getLinkAttribute(ENResultsAPI* enrapi, int timeIndex,
+int DLLEXPORT ENR_getLinkAttribute(ENResultsAPI* enrapi, int periodIndex,
         ENR_LinkAttribute attr, float* outValueArray);
-
-int DLLEXPORT ENR_getNodeResult(ENResultsAPI* enrapi, int timeIndex, int nodeIndex,
+int DLLEXPORT ENR_getNodeResult(ENResultsAPI* enrapi, int periodIndex, int nodeIndex,
         float* outValueArray);
-int DLLEXPORT ENR_getLinkResult(ENResultsAPI* enrapi, int timeIndex, int linkIndex,
+int DLLEXPORT ENR_getLinkResult(ENResultsAPI* enrapi, int periodIndex, int linkIndex,
         float* outValueArray);
-
 int DLLEXPORT ENR_free(float *array);
-int DLLEXPORT ENR_close(ENResultsAPI **enrapi);
+
+int DLLEXPORT ENR_close(ENResultsAPI* enrapi);
+
 int DLLEXPORT ENR_errMessage(int errcode, char* errmsg, int n);
 
 

74cf5a44fc53f54383c0447f7371526ab48ae4e9
Mark Gray <markgray@aquaterra.com>
2016-07-11 09:25:04 -0400

    refactor:     add documentation     count folders, files, lines

--- src/core/refactor.py
+++ src/core/refactor.py
@@ -3,12 +3,21 @@ import sys
 import errno
 import re
 
+"""
+Refactor existing code that contains class definitions with reader and writer methods
+by moving reader methods all into a new file "inp_file_reader.py"
+and moving writer methods all into a new file "inp_file_writer.py"
+
+This version does not handle re-writing code that accesses the reader and writer methods.
+"""
+
 reader_method = "set_text"
 writer_method = "get_text"
 reader_class_prefix = "Read"
 writer_class_prefix = "Write"
 top_dir = "C:\\devNotMW\\SWMM-EPANET_User_Interface_dev_ui\\src\\"
-start_dir = top_dir + "core\\swmm"
+# start_dir = top_dir + "core\\swmm"
+start_dir = top_dir + "core\\epanet"
 
 original_subpath = "\\src\\"
 refactor_subpath = "\\src-refactor\\"
@@ -20,6 +29,7 @@ imports = ["import traceback",
 
 
 def un_camel(name):
+    """ Take ClassNameLikeThis and return class_name_like_this, for converting a class name into a variable name. """
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
@@ -31,11 +41,17 @@ try:
 except:
     pass
 
-count_edits = 0
+folders_read = 0
+files_read = 0
+files_edited = 0
+lines_read = 0
+edits_made = 0
+class_header_edits_made = 0
 reader_contents = []
 writer_contents = []
 for root, subdirs, files in os.walk(start_dir):
-    print('--\nroot = ' + root)
+    print('\nreading folder ' + root)
+    folders_read += 1
 
     for filename in files:
         file_base, ext = os.path.splitext(filename)
@@ -43,7 +59,9 @@ for root, subdirs, files in os.walk(start_dir):
             file_path = os.path.join(root, filename)
             with open(file_path, 'rb') as f:
                 file_contents = f.read()
+                files_read += 1
             if reader_method in file_contents or writer_method in file_contents:
+                files_edited += 1
                 rewrite_filename = os.path.join(root.replace(original_subpath, refactor_subpath), file_base) + ".py"
                 rewrite_dir = os.path.dirname(rewrite_filename)
                 try:
@@ -52,16 +70,18 @@ for root, subdirs, files in os.walk(start_dir):
                     if exception.errno != errno.EEXIST:
                         raise
                 with open(rewrite_filename, 'wb') as rewrite_file:
-                    print('\t- file %s (full path: %s)' % (filename, file_path))
+                    # print('\t- file %s (full path: %s)' % (filename, file_path))
                     file_import_path = os.path.join(root, file_base)[len(top_dir):].replace('\\', '.')
                     reader_class_header = []
                     writer_class_header = []
+                    class_header_edits_made = 0
                     in_class = False
                     in_read_method = False
                     in_write_method = False
                     in_other_method = False
                     class_var_name = "self"
                     for line in file_contents.splitlines():
+                        lines_read += 1
                         fields = re.split(r'[\s,\(,\:]', line)
                         try:
                             class_index = fields.index("class")
@@ -75,21 +95,32 @@ for root, subdirs, files in os.walk(start_dir):
                                 in_read_method = False
                                 in_write_method = False
                                 in_other_method = False
-                                rewrite_file.write(line + '\n')
+
+                                rewrite_line = line.replace("(Section)", '') + '\n'
+                                if ("Section" in rewrite_line and not "CrossSection" in rewrite_line
+                                     and not "XSECTION" in rewrite_line) or\
+                                    "InputFile" in rewrite_line:
+                                    print(rewrite_line)
+                                rewrite_file.write(rewrite_line)
+                                if rewrite_line != line:
+                                    edits_made += 1
+
                                 if "Enum" in line:
                                     reader_class_header = []
                                     writer_class_header = []
+                                    class_header_edits_made = 0
                                 else:
                                     in_class = True
                                     reader_class_name = reader_class_prefix + orig_class_name
                                     writer_class_name = writer_class_prefix + orig_class_name
                                     reader_class_header = ["\n", line.replace(orig_class_name, reader_class_name)]
                                     writer_class_header = ["\n", line.replace(orig_class_name, writer_class_name)]
+                                    class_header_edits_made = 1
                                 line = None
                         except Exception as ex:
                             if str(ex) != "'class' is not in list":
-                                print str(ex)
-                                print line
+                                print(str(ex))
+                                print(line)
 
                         if line is not None:
                             if " def " in line:
@@ -105,7 +136,8 @@ for root, subdirs, files in os.walk(start_dir):
                                         writer_contents.extend(writer_class_header)
                                         writer_class_header = []
                                     line = "    @staticmethod\n" + line
-                                    count_edits += 1
+                                    edits_made += 1 + class_header_edits_made
+                                    class_header_edits_made = 0
                                 else:
                                     in_read_method = False
                                     in_write_method = False
@@ -118,23 +150,27 @@ for root, subdirs, files in os.walk(start_dir):
                                 else:
                                     writer_class_header.append(new_line)
                                 line = None  # discard this line before it is written to reader or rewrite
-                                count_edits += 1
+                                # edits_made += 1
 
                             if line is not None:
                                 # Replace references to "self" with references to argument class_var_name.
                                 new_line = line.replace("self", class_var_name)
-                                if new_line != line:
-                                    count_edits += 1
 
                                 if in_read_method:
                                     reader_contents.append(new_line)
+                                    if new_line != line:
+                                        edits_made += 1
                                 elif in_write_method:
                                     writer_contents.append(new_line)
+                                    if new_line != line:
+                                        edits_made += 1
                                 elif in_class and not in_other_method:
                                     # add this line to class headers, to be written if and when
                                     # a reader or writer method is found in this class.
                                     reader_class_header.append(new_line)
                                     writer_class_header.append(new_line)
+                                    if new_line != line:
+                                        class_header_edits_made += 1
                                 if not in_read_method and not in_write_method:
                                     # write this line in new version of original file
                                     # unless it is in method to be moved.
@@ -148,7 +184,7 @@ for root, subdirs, files in os.walk(start_dir):
                                             print rewrite_line
                                         rewrite_file.write(rewrite_line)
                                         if rewrite_line != line:
-                                            count_edits += 1
+                                            edits_made += 1
 
 if writer_contents:
     with open(writer_path, 'wb') as writer_file:
@@ -160,4 +196,9 @@ if reader_contents:
         reader_file.write('\n'.join(imports) + '\n\n')
         reader_file.write('\n'.join(reader_contents) + '\n\n')
 
-print "Counted edits: " + str(count_edits) + " written to " + start_dir.replace(original_subpath, refactor_subpath)
+print("\nFolders read: " + str(folders_read))
+print("Files read: " + str(files_read))
+print("Files edited: " + str(files_edited))
+print("Lines in edited files: " + str(lines_read))
+print("Edits made: " + str(edits_made))
+print("Refactored classes written to " + start_dir.replace(original_subpath, refactor_subpath))

62f779b23f763bbea0829fc430a00e9ca7d806dc
Mark Gray <markgray@aquaterra.com>
2016-08-12 07:52:18 -0400

    New plugin now demonstrate saving EPANET pipes to GeoJSON

--- src/plugins/SaveEpanetGIS/__init__.py
+++ src/plugins/SaveEpanetGIS/__init__.py
@@ -5,28 +5,34 @@ try:
     from PyQt4.QtGui import QMessageBox
     import os
 
-    plugin_name = "Save EPANET to GIS"
+    plugin_name = "SaveEpanetGIS"  # "Save EPANET to GIS"
     plugin_create_menu = True
-    __all__ = {"Save Pipes": "pipes", "Save Pumps": "pumps", "": 3}
+    # __all__ = {"Save Pipes": "pipes", "Save Pumps": "pumps"}
+    __all__ = {"Save Pipes": 1, "Save Pumps": 2}
 
 
     def run(session=None, choice=None):
-        if choice == "pipes":
-            if not session or not session.project:
-                result = "Project is not open"
-            else:
-                file_name = os.path.join(os.path.dirname(session.project.file_name), "pipes.json")
-                links = session.project.pipes.value
-                attributes = ["name", "description", "inlet_node", "outlet_node", "length", "diameter", "roughness",
-                              "loss_coefficient"]
-                save_links(session.project, links, file_name, attributes)
-                result = "Saved " + file_name
+        print("run " + str(choice))
+        try:
+            if choice == 1:
+                if not session or not session.project:
+                    result = "Project is not open"
+                else:
+                    file_name = os.path.join(os.path.dirname(session.project.file_name), "pipes.json")
+                    print("save pipes to " + file_name)
+                    links = session.project.pipes.value
+                    attributes = ["name", "description", "inlet_node", "outlet_node", "length", "diameter", "roughness",
+                                  "loss_coefficient"]
+                    save_links(session.project, links, file_name, attributes)
+                    result = "Saved " + file_name
 
-        #elif choice == "pumps":
-        #elif choice == 3:
-        else:
-            result = "Selected operation not yet implemented."
-        QMessageBox.information(None, plugin_name, result, QMessageBox.Ok)
+            #elif choice == "pumps":
+            #elif choice == 3:
+            else:
+                result = "Selected operation not yet implemented."
+            QMessageBox.information(None, plugin_name, result, QMessageBox.Ok)
+        except Exception as ex:
+            print str(ex)
 
     def save_links(project, links, file_name, attributes):
         coordinates = project.coordinates.value
@@ -46,15 +52,16 @@ try:
                     inlet_coord = coordinate_pair
                 if coordinate_pair.name == link.outlet_node:
                     outlet_coord = coordinate_pair
-            if inlet_coord and outlet_coord:
-                # add a feature
-                feature = QgsFeature()
-                feature.setGeometry(QgsGeometry.fromPolyline([
-                    QgsPoint(float(inlet_coord.x), float(inlet_coord.y)),
-                    QgsPoint(float(outlet_coord.x), float(outlet_coord.y))]))
+                if inlet_coord and outlet_coord:
+                    # add a feature
+                    feature = QgsFeature()
+                    feature.setGeometry(QgsGeometry.fromPolyline([
+                        QgsPoint(float(inlet_coord.x), float(inlet_coord.y)),
+                        QgsPoint(float(outlet_coord.x), float(outlet_coord.y))]))
 
-                feature.setAttributes([getattr(link, link_attr, '') for link_attr in attributes])
-                features.append(feature)
+                    feature.setAttributes([getattr(link, link_attr, '') for link_attr in attributes])
+                    features.append(feature)
+                    break  # stop looking for more coordinates
 
         # changes are only possible when editing the layer
         layer.startEditing()

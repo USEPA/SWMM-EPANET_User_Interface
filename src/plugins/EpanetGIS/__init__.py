1c440b78e043a1f98aaa023f76ffb2ab4c6eb2a0
Mark Gray <markgray@aquaterra.com>
2016-08-14 23:16:23 -0400

    EpanetGIS plugin (renamed from SaveEpanetGIS) can now save and load pipes to/from GeoJSON.

---
+++ src/plugins/EpanetGIS/__init__.py
@@ -0,0 +1,134 @@
+try:
+    from qgis.core import *
+    from qgis.gui import *
+    from PyQt4 import QtGui, QtCore, Qt
+    from PyQt4.QtGui import QMessageBox
+    from core.epanet.hydraulics.link import Pipe
+    import os
+
+    plugin_name = "EpanetGIS"  # "Save EPANET to GIS"
+    plugin_create_menu = True
+    # __all__ = {"Save Pipes": "pipes", "Save Pumps": "pumps"}
+    __all__ = {"Save Pipes to GIS": 1, "Load Pipes from GIS": 2}
+
+
+    def run(session=None, choice=None):
+        print("run " + str(choice))
+        try:
+            if choice == 1:
+                if not session or not session.project:
+                    result = "Project is not open"
+                else:
+                    file_name = os.path.join(os.path.dirname(session.project.file_name), "pipes.json")
+                    print("save pipes to " + file_name)
+                    links = session.project.pipes.value
+                    attributes = ["name", "description", "inlet_node", "outlet_node", "length", "diameter", "roughness",
+                                  "loss_coefficient"]
+                    save_links(session.project, links, file_name, attributes)
+                    result = "Saved " + file_name
+
+            elif choice == 2:
+                if not session or not session.project:
+                    result = "Project is not open"
+                else:
+                    gui_settings = QtCore.QSettings("EPANET", "GUI")
+                    directory = gui_settings.value("ImportGIS", "")
+                    file_name = QtGui.QFileDialog.getOpenFileName(session, "Select GIS file to import", directory,
+                                                                  "All files (*.*)")
+                    if file_name:
+                        path_only, file_only = os.path.split(file_name)
+                        if path_only != directory:
+                            gui_settings.setValue("ImportGIS", path_only)
+                            gui_settings.sync()
+                            del gui_settings
+
+                        section = session.project.pipes
+
+                        if not type(section.value) is list:
+                            section.value = []
+                        elif len(section.value) > 0:
+                            msg = QMessageBox()
+                            msg.setIcon(QMessageBox.Question)
+                            msg.setText("Discard " + len(items) + " pipes in model before import?")
+                            msg.setWindowTitle("Importing Pipes")
+                            msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+                            retval = msg.exec_()
+                            print("value of pressed message box button:", retval)
+
+                        attributes = {"name": "name",
+                                      "description": "description",
+                                      "inlet_node": "inlet_node",
+                                      "outlet_node": "outlet_node",
+                                      "length": "length",
+                                      "diameter": "diameter",
+                                      "roughness": "roughness",
+                                      "loss_coefficient": "loss_coefficient"}
+                        """ Dictionary of attribute names in vector layer: attribute names of EPANET object. """
+
+                        result = load_links(section.value, file_name, attributes, Pipe)
+            else:
+                result = "Selected operation not yet implemented."
+            QMessageBox.information(None, plugin_name, result, QMessageBox.Ok)
+        except Exception as ex:
+            print str(ex)
+
+    def load_links(links, file_name, attributes, model_type):
+        count = 0
+        try:
+            layer = QgsVectorLayer(file_name, "import", "ogr")
+            if layer:
+                for feature in layer.getFeatures():
+                    model_item = model_type()
+                    for layer_attribute, model_attribute in attributes.items():
+                        attr_value = feature[layer_attribute]
+                        setattr(model_item, model_attribute, attr_value)
+                    links.append(model_item)
+                    count += 1
+        except Exception as ex:
+            print(str(ex))
+        return "Imported " + str(count) + " features"
+
+    def save_links(project, links, file_name, attributes):
+        coordinates = project.coordinates.value
+        layer = QgsVectorLayer("LineString", "links", "memory")
+        provider = layer.dataProvider()
+
+        # add fields
+        provider.addAttributes([QgsField(link_attr, QtCore.QVariant.String) for link_attr in attributes])
+
+        features = []
+        # Receivers = as in the above example 'Receivers' is a list of results
+        for link in links:
+            inlet_coord = None
+            outlet_coord = None
+            for coordinate_pair in coordinates:
+                if coordinate_pair.name == link.inlet_node:
+                    inlet_coord = coordinate_pair
+                if coordinate_pair.name == link.outlet_node:
+                    outlet_coord = coordinate_pair
+                if inlet_coord and outlet_coord:
+                    # add a feature
+                    feature = QgsFeature()
+                    feature.setGeometry(QgsGeometry.fromPolyline([
+                        QgsPoint(float(inlet_coord.x), float(inlet_coord.y)),
+                        QgsPoint(float(outlet_coord.x), float(outlet_coord.y))]))
+
+                    feature.setAttributes([getattr(link, link_attr, '') for link_attr in attributes])
+                    features.append(feature)
+                    break  # stop looking for more coordinates
+
+        # changes are only possible when editing the layer
+        layer.startEditing()
+        provider.addFeatures(features)
+        layer.commitChanges()
+        layer.updateExtents()
+
+        if file_name.lower().endswith("shp"):
+            driver_name = "GeoJson"
+        else:
+            driver_name = "GeoJson"
+        QgsVectorFileWriter.writeAsVectorFormat(layer, file_name, "utf-8", layer.crs(), driver_name)
+
+
+except Exception as ex:
+    print "Skip loading plugin: " + str(ex)

3ee12129f787f43deb022f663be54e1d73079f13
xinyuwei <xinyuwei@gmail.com>
2016-08-31 16:53:30 -0400

    title test, updated read, write, and match method

--- test/core/epanet/test_title.py
+++ test/core/epanet/test_title.py
@@ -1,4 +1,7 @@
 from core.epanet.title import Title
+from core.epanet.inp_reader_sections import TitleReader
+from core.epanet.inp_writer_sections import TitleWriter
+from test.core.section_match import match
 import unittest
 
 
@@ -11,30 +14,33 @@ class SimpleTitleTest(unittest.TestCase):
     def test_bare(self):
         """Bare section"""
         self.my_title = Title()
-        default_text = self.my_title.get_text()
+        # default_text = self.my_title.get_text()
+        default_text = TitleWriter.as_text(self.my_title)
+
         test_text = ""
-        self.my_title.set_text(test_text)
-        actual_text = self.my_title.get_text()
+        # self.my_title.set_text(test_text)
+        self.my_title = TitleReader.read(test_text)
+        actual_text = TitleWriter.as_text(self.my_title)
         # assert actual_text == test_text
         assert actual_text == default_text
-        # assert self.my_title.matches(test_text)
 
     def test_empty(self):
         """Empty section (has section name)"""
         self.my_title = Title()
         test_text = "[TITLE]\n"
-        self.my_title.set_text(test_text)
-        actual_text = self.my_title.get_text()  # display purpose
-        assert self.my_title.matches(test_text)
+        self.my_title = TitleReader.read(test_text)
+        actual_text = TitleWriter.as_text(self.my_title)
+        # assert self.my_title.matches(test_text)
+        assert match(actual_text, test_text)
 
     def test_one_row(self):
         """One-row title with carriage return"""
         self.my_title = Title()
         test_text = "[TITLE]\n" \
                     "test_title\n"
-        self.my_title.set_text(test_text)
-        actual_text = self.my_title.get_text()  # display purpose
-        assert self.my_title.matches(test_text)
+        self.my_title = TitleReader.read(test_text)
+        actual_text = TitleWriter.as_text(self.my_title)
+        assert match(actual_text, test_text)
 
     def test_multi_row(self):
         """Multiple-row title include empty lines"""
@@ -43,9 +49,9 @@ class SimpleTitleTest(unittest.TestCase):
                     "       \n" \
                     "test_title\n" \
                     "    "
-        self.my_title.set_text(test_text)
-        actual_text = self.my_title.get_text()  # display purpose
-        assert self.my_title.matches(test_text)
+        self.my_title = TitleReader.read(test_text)
+        actual_text = TitleWriter.as_text(self.my_title)
+        assert match(actual_text, test_text)
 
     def test_rt_before_title(self):
         """Carriage return before section title"""
@@ -54,11 +60,6 @@ class SimpleTitleTest(unittest.TestCase):
         test_text = "\n"\
                     "[TITLE]\n"\
                     "test_title"
-        self.my_title.set_text(test_text)
-        actual_text = self.my_title.get_text()  # display purpose
-        assert self.my_title.matches(test_text)
-
-
-
-
-
+        self.my_title = TitleReader.read(test_text)
+        actual_text = TitleWriter.as_text(self.my_title)
+        assert match(actual_text, test_text)

7fb208175ced4669971a557dfa87d041fec010e9
tong.zhai <tong.zhai@respec.com>
2016-09-27 09:33:54 -0400

    removed redundant widget from tabCorrelation tab

--- src/ui/EPANET/frmCalibrationReportDesigner.py
+++ src/ui/EPANET/frmCalibrationReportDesigner.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\dev\Python\dev-ui\src\ui\EPANET\frmCalibrationReportDesigner.ui'
+# Form implementation generated from reading ui file 'frmCalibrationReportDesigner.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created: Tue Sep 27 01:04:33 2016
+#      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -32,24 +33,18 @@ class Ui_frmCalibrationReport(object):
         self.centralWidget = QtGui.QWidget(frmCalibrationReport)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
         self.verticalLayout_8 = QtGui.QVBoxLayout(self.centralWidget)
-        self.verticalLayout_8.setMargin(11)
-        self.verticalLayout_8.setSpacing(6)
         self.verticalLayout_8.setObjectName(_fromUtf8("verticalLayout_8"))
         self.fraTop = QtGui.QFrame(self.centralWidget)
         self.fraTop.setFrameShape(QtGui.QFrame.StyledPanel)
         self.fraTop.setFrameShadow(QtGui.QFrame.Raised)
         self.fraTop.setObjectName(_fromUtf8("fraTop"))
         self.horizontalLayout_3 = QtGui.QHBoxLayout(self.fraTop)
-        self.horizontalLayout_3.setMargin(11)
-        self.horizontalLayout_3.setSpacing(6)
         self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
         self.tabWidget = QtGui.QTabWidget(self.fraTop)
         self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
         self.tabStatistics = QtGui.QWidget()
         self.tabStatistics.setObjectName(_fromUtf8("tabStatistics"))
         self.verticalLayout_4 = QtGui.QVBoxLayout(self.tabStatistics)
-        self.verticalLayout_4.setMargin(11)
-        self.verticalLayout_4.setSpacing(6)
         self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.txtStatistics = QtGui.QTextEdit(self.tabStatistics)
         font = QtGui.QFont()
@@ -60,19 +55,10 @@ class Ui_frmCalibrationReport(object):
         self.tabWidget.addTab(self.tabStatistics, _fromUtf8(""))
         self.tabCorrelation = QtGui.QWidget()
         self.tabCorrelation.setObjectName(_fromUtf8("tabCorrelation"))
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.tabCorrelation)
-        self.horizontalLayout_2.setMargin(11)
-        self.horizontalLayout_2.setSpacing(6)
-        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
-        self.widgetPlot = QtGui.QWidget(self.tabCorrelation)
-        self.widgetPlot.setObjectName(_fromUtf8("widgetPlot"))
-        self.horizontalLayout_2.addWidget(self.widgetPlot)
         self.tabWidget.addTab(self.tabCorrelation, _fromUtf8(""))
         self.tabMean = QtGui.QWidget()
         self.tabMean.setObjectName(_fromUtf8("tabMean"))
         self.horizontalLayout_4 = QtGui.QHBoxLayout(self.tabMean)
-        self.horizontalLayout_4.setMargin(11)
-        self.horizontalLayout_4.setSpacing(6)
         self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
         self.widgetMean = QtGui.QWidget(self.tabMean)
         self.widgetMean.setObjectName(_fromUtf8("widgetMean"))
@@ -85,8 +71,6 @@ class Ui_frmCalibrationReport(object):
         self.fraOKCancel.setFrameShadow(QtGui.QFrame.Raised)
         self.fraOKCancel.setObjectName(_fromUtf8("fraOKCancel"))
         self.horizontalLayout = QtGui.QHBoxLayout(self.fraOKCancel)
-        self.horizontalLayout.setMargin(11)
-        self.horizontalLayout.setSpacing(6)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         spacerItem = QtGui.QSpacerItem(338, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
@@ -97,7 +81,7 @@ class Ui_frmCalibrationReport(object):
         frmCalibrationReport.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(frmCalibrationReport)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(frmCalibrationReport)
 
     def retranslateUi(self, frmCalibrationReport):

e732c8447076fd18b21315a8439d8e1572cb5c68
tong.zhai <tong.zhai@respec.com>
2016-10-19 12:59:51 -0400

    settings: internationalization class handles loading correct languages frmMainSWMM.py: set up internationalization frmMainEPANET.py: set up internationalization

---
+++ src/ui/settings.py
@@ -0,0 +1,19 @@
+from PyQt4.QtCore import QTranslator
+from PyQt4.QtGui import QInputDialog
+from ui.translations_rc import *
+class internationalization:
+    def __init__(self):
+        self.ui_languages = ("English", "French", "Chinese")
+        self.ui_language = "English"
+        self.ui_gTranslator = QTranslator()
+        self.ui_mTranslator = QTranslator()
+
+    def set_language(self):
+        lang = QInputDialog.getItem(None, "Select Language", "Language", self.ui_languages)
+        self.ui_language = lang[0]
+        if self.ui_language == "French":
+            self.ui_mTranslator.load(":/french.qm")
+            self.ui_gTranslator.load(':/qt_fr.qm')
+        elif self.ui_language == "Chinese":
+            self.ui_mTranslator.load(":/chinese.qm")
+            self.ui_gTranslator.load(':/qt_zh_CN.qm')

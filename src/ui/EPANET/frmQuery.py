8cb10ec1022721b14aedcc3163cc4869f8bd6285
Paul.Duda <paul.duda@respec.com>
2017-03-24 17:01:40 -0400

    Fix small differences in unit tests

--- src/ui/EPANET/frmQuery.py
+++ src/ui/EPANET/frmQuery.py
@@ -2,13 +2,16 @@ from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import *
 from ui.help import HelpHandler
 from frmQueryDesigner import Ui_frmQuery
+import Externals.epanet.outputapi.ENOutputWrapper as ENO
+from core.epanet.hydraulics.node import Junction
+from core.epanet.hydraulics.node import Reservoir
+from core.epanet.hydraulics.node import Tank
+from core.epanet.hydraulics.link import Pipe
+from core.epanet.hydraulics.link import Pump
+from core.epanet.hydraulics.link import Valve
 
 
 class frmQuery(QtGui.QMainWindow, Ui_frmQuery):
-
-    TXT_NODES_WITH = 'Find Nodes with'
-    TXT_LINKS_WITH = 'Find Links with'
-    TXT_ITEMS_FOUND = ' items found'
 
     def __init__(self, session, project):
         QtGui.QMainWindow.__init__(self, session)
@@ -16,170 +19,124 @@ class frmQuery(QtGui.QMainWindow, Ui_frmQuery):
         self.setupUi(self)
         self.session = session
         self.project = project
-# ComboBox1.Items.Add(TXT_NODES_WITH);
-#   ComboBox1.Items.Add(TXT_LINKS_WITH);
-#   ComboBox1.ItemIndex := 0;
-#
-# // Call OnChange handler to load appropriate variables
-# // into combo box that selects a variable to query
-#   ComboBox1Change(self);
-#
-# // Load the choices of relations to query on (below, equals, above)
-#   for I := Low(FilterRelation) to High(FilterRelation) do
-#     ComboBox3.Items.Add(FilterRelation[I]);
-#   ComboBox3.ItemIndex := 0;
-#   OldNodeVar := -1;
-#   OldLinkVar := -1;
-#
-# // Position form at top left of Main form
-#   with MainForm do
-#   begin
-#     P.x := Left + (Width - ClientWidth) - 2;
-#     P.Y := Top + (Height-ClientHeight) - 2;
-#   end;
-#   Top := P.Y;
-#   Left := P.X;
 
+        self.cboFind.currentIndexChanged.connect(self.cboFind_Changed)
+        QtCore.QObject.connect(self.cmdSubmit, QtCore.SIGNAL("clicked()"), self.cmdSubmit_Clicked)
 
-# procedure TQueryForm.FormShow(Sender: TObject);
-# //---------------------------------------
-# // OnShow handler for the form.
-# //---------------------------------------
-# begin
-# // Set form's font
-#   Uglobals.SetFont(self);
-#   Panel1.Font.Color := clRed;
-#
-# // Make sure map will display junction symbols
-#   with MapForm.Map.Options do
-#   begin
-#     OldDispJuncs := DispJuncs;
-#     DispJuncs := True;
-#   end;
-# end;
-#
-# procedure TQueryForm.FormClose(Sender: TObject; var Action: TCloseAction);
-# //-------------------------------------------------------------------------
-# // OnClose handler for form.
-# // Restores the map display to the state it had before last query was made.
-# //-------------------------------------------------------------------------
-# begin
-#   if (QueryFlag) then
-#   begin
-#     QueryFlag := False;
-#     CurrentNodeVar := OldNodeVar;
-#     CurrentLinkVar := OldLinkVar;
-#     OldNodeVar := -1;
-#     OldLinkVar := -1;
-#     Uoutput.SetNodeColors;
-#     Uoutput.SetLinkColors;
-#     MapForm.Map.Options.DispJuncs := OldDispJuncs;
-#     MapForm.RedrawMap;
-#     MapForm.DrawNodeLegend;
-#     MapForm.DrawLinkLegend;
-#   end;
-#   BrowserForm.NodeViewBox.Enabled := True;
-#   BrowserForm.LinkViewBox.Enabled := True;
-#   Action := caHide;
-# end;
-#
-# procedure TQueryForm.Clear;
-# //-----------------------------------------------
-# // Clears Query Value and Results fields of form.
-# //-----------------------------------------------
-# begin
-#   Edit1.Text := '';
-#   Panel1.Caption := '';
-# end;
-#
-# procedure TQueryForm.ComboBox1Change(Sender: TObject);
-# //----------------------------------------------------
-# // OnChange handler for the Node/Link combo box.
-# // Changes list of map display variables to choose
-# // from when user switches between Node & Link query.
-# //----------------------------------------------------
-# var
-#   i: Integer;
-# begin
-#   ComboBox2.Clear;
-#   if ComboBox1.ItemIndex = 0 then
-#     for i := ELEVATION to NODEQUAL do
-#       ComboBox2.Items.Add(NodeVariable[i].Name)
-#   else
-#     for i := LINKLENGTH to LINKQUAL do
-#       ComboBox2.Items.Add(LinkVariable[i].Name);
-#   ComboBox2.ItemIndex := 0;
-# end;
-#
-# procedure TQueryForm.Button1Click(Sender: TObject);
-# //----------------------------------------------------------------
-# // OnClick handler for the command button that executes the query.
-# //----------------------------------------------------------------
-# begin
-# // Check for valid numerical entry for query comparison value
-#   if Uutils.IsValidNumber(Edit1.Text,QueryValue) then
-#   begin
-#
-#   // Set a flag & save query relation type
-#     QueryFlag := True;
-#     QueryRelation := TRelationType(ComboBox3.ItemIndex);
-#
-#   // Disable selection of map view variable from Browser form
-#     BrowserForm.NodeViewBox.Enabled := False;
-#     BrowserForm.LinkViewBox.Enabled := False;
-#
-#   // If this is first query since form was shown again then save
-#   // current status of map view
-#     if (OldNodeVar < 0) then OldNodeVar := CurrentNodeVar;
-#     if (OldLinkVar < 0) then OldLinkVar := CurrentLinkVar;
-#
-#   // Select the query variable for viewing on the map
-#     CurrentLinkVar := NOVIEW;
-#     CurrentNodeVar := NOVIEW;
-#     if (ComboBox1.ItemIndex = 0) then
-#     begin
-#       CurrentNodeVar := ComboBox2.ItemIndex+1;
-#       Uoutput.SetNodeColors;
-#     end
-#     else
-#     begin
-#       CurrentLinkVar := ComboBox2.ItemIndex+1;
-#       Uoutput.SetLinkColors;
-#     end;
-#
-#   // Display number of items matching the query
-#     UpdateQueryCaption;
-#
-#   // Redraw the map
-#     MapForm.RedrawMap;
-#     MapForm.DrawNodeLegend;
-#     MapForm.DrawLinkLegend;
-#   end
-#   else Edit1.SetFocus;
-# end;
-#
-# procedure TQueryForm.UpdateQueryCaption;
-# //----------------------------------------------------------------
-# // Updates the display of number of items matching the query.
-# //----------------------------------------------------------------
-# var
-#   I,J,N: Integer;
-# begin
-#   Panel1.Caption := '';
-#   N := 0;
-#   if CurrentNodeVar <> NOVIEW then
-#   begin
-#       for I := JUNCS to TANKS do
-#         for J := 0 to Network.Lists[I].Count-1 do
-#           if Node(I,J).ColorIndex > 0 then Inc(N);
-#   end
-#   else if CurrentLinkVar <> NOVIEW then
-#   begin
-#       for I := PIPES to VALVES do
-#         for J := 0 to Network.Lists[I].Count-1 do
-#           if Link(I,J).ColorIndex > 0 then Inc(N);
-#   end;
-#   Panel1.Caption := IntToStr(N) + TXT_ITEMS_FOUND;
-# end;
-#
-# end.
+        self.cboFind.addItem('Find Nodes with')
+        self.cboFind.addItem('Find Links with')
+
+        self.cboAbove.addItem('Below')
+        self.cboAbove.addItem('Equal To')
+        self.cboAbove.addItem('Above')
+
+    def cboFind_Changed(self):
+        # Changes list of map display variables to choose
+        # from when user switches between Node & Link query.
+        self.cboProperty.clear()
+        if self.cboFind.currentIndex() == 0:
+            self.cboProperty.addItems(['Elevation','Base Demand','Initial Quality'])
+            if self.session.output:
+                object_type = ENO.ENR_node_type
+                if object_type:
+                    attribute_names = [attribute.name for attribute in object_type.Attributes]
+                    for item in attribute_names:
+                        self.cboProperty.addItem(item)
+        else:
+            self.cboProperty.addItems(['Length','Diameter','Roughness','Bulk Coeff.','Wall Coeff.'])
+            if self.session.output:
+                object_type = ENO.ENR_link_type
+                if object_type:
+                    attribute_names = [attribute.name for attribute in object_type.Attributes]
+                    for item in attribute_names:
+                        self.cboProperty.addItem(item)
+
+    def cmdSubmit_Clicked(self):
+        val = float(self.txtNum.text())
+        selected_attribute = self.cboProperty.currentText()
+        setting_index = self.cboProperty.currentIndex()
+
+        count = 0
+        if self.cboFind.currentIndex() == 0:
+            attribute = None
+            if setting_index < 3:
+                meta_item = Junction.metadata.meta_item_of_label(selected_attribute)
+                attribute = meta_item.attribute
+                if attribute:  # Found an attribute of the node class to color by
+                    for item in self.project.all_nodes():
+                        value = float(getattr(item, attribute, 0))
+                        if self.cboAbove.currentIndex() == 0:
+                            # below
+                            if value < val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 1:
+                            # equal to
+                            if value == val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 2:
+                            # above
+                            if value > val:
+                                count += 1
+            elif self.session.output:  # Look for attribute to color by in the output
+                attribute = ENO.ENR_node_type.get_attribute_by_name(selected_attribute)
+                if attribute:
+                    values = ENO.ENR_node_type.get_attribute_for_all_at_time(self.session.output, attribute, self.session.time_index)
+                    index = 0
+                    for node in self.session.output.nodes.values():
+                        value = values[index]
+                        index += 1
+                        if self.cboAbove.currentIndex() == 0:
+                            # below
+                            if value < val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 1:
+                            # equal to
+                            if value == val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 2:
+                            # above
+                            if value > val:
+                                count += 1
+        else:
+            attribute = None
+            if setting_index < 5:
+                meta_item = Pipe.metadata.meta_item_of_label(selected_attribute)
+                attribute = meta_item.attribute
+                if attribute:  # Found an attribute of the pipe class to color by
+                    for link in self.project.all_links():
+                        value = float(getattr(link, attribute, 0))
+                        if self.cboAbove.currentIndex() == 0:
+                            # below
+                            if value < val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 1:
+                            # equal to
+                            if value == val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 2:
+                            # above
+                            if value > val:
+                                count += 1
+            elif self.session.output:  # Look for attribute to color by in the output
+                attribute = ENO.ENR_link_type.get_attribute_by_name(selected_attribute)
+                if attribute:
+                    values = ENO.ENR_link_type.get_attribute_for_all_at_time(self.session.output, attribute, self.session.time_index)
+                    index = 0
+                    for link in self.session.output.links.values():
+                        value = values[index]
+                        index += 1
+                        if self.cboAbove.currentIndex() == 0:
+                            # below
+                            if value < val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 1:
+                            # equal to
+                            if value == val:
+                                count += 1
+                        elif self.cboAbove.currentIndex() == 2:
+                            # above
+                            if value > val:
+                                count += 1
+
+        # Display number of items matching the query
+        self.txtSummary.setText(str(count) + ' items found')

656fe5e4798fb056bddb569a13e764d036c79cee
PaulDudaRESPEC <paul.duda@respec.com>
2018-07-24 14:32:05 -0400

    frmMainSWMM -- remove View:Objects menu because that functionality is done through the Layers tab on the main form.  Resolves issue #160.

--- src/ui/SWMM/frmMainSWMM.py
+++ src/ui/SWMM/frmMainSWMM.py
@@ -341,6 +341,7 @@ class frmMainSWMM(frmMain):
         self.actionStdProjSummary.triggered.connect(self.show_summary)
         self.actionStdProjDetails.triggered.connect(self.show_details)
         self.menuProject.removeAction(self.actionStdProjSimulation_Options)  # remove menus that are EPANET-specific
+        self.menuObjects.deleteLater()
 
         self.actionTranslate_Coordinates = QAction(self)
         self.actionTranslate_Coordinates.setObjectName(from_utf8("actionTranslate_CoordinatesMenu"))

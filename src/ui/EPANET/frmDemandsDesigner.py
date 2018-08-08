837ed9a4afcf8b9c69b52c6196cab50170ee79e6
TongZhai <tong.zhai@respec.com>
2018-08-08 02:10:29 -0400

    qt5 update EPANET forms

--- src/ui/EPANET/frmDemandsDesigner.py
+++ src/ui/EPANET/frmDemandsDesigner.py
@@ -1,76 +1,61 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\dev\Python\dev-ui\src\ui\EPANET\frmDemandsDesigner.ui'
+# Form implementation generated from reading ui file 'EPANET\frmDemandsDesigner.ui'
 #
-# Created by: PyQt5 UI code generator 4.11.4
+# Created by: PyQt5 UI code generator 5.9
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import *
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_frmDemands(object):
     def setupUi(self, frmDemands):
-        frmDemands.setObjectName(_fromUtf8("frmDemands"))
-        frmDemands.resize(295, 265)
+        frmDemands.setObjectName("frmDemands")
+        frmDemands.resize(484, 356)
         font = QtGui.QFont()
         font.setPointSize(10)
         frmDemands.setFont(font)
-        self.centralWidget = QWidget(frmDemands)
-        self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
-        self.verticalLayout = QVBoxLayout(self.centralWidget)
-        # self.verticalLayout.setMargin(11)
+        self.centralWidget = QtWidgets.QWidget(frmDemands)
+        self.centralWidget.setObjectName("centralWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralWidget)
+        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
         self.verticalLayout.setSpacing(6)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.fraTop = QFrame(self.centralWidget)
-        self.fraTop.setFrameShape(QFrame.StyledPanel)
-        self.fraTop.setFrameShadow(QFrame.Raised)
-        self.fraTop.setObjectName(_fromUtf8("fraTop"))
-        self.gridLayout = QGridLayout(self.fraTop)
-        # self.gridLayout.setMargin(11)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.fraTop = QtWidgets.QFrame(self.centralWidget)
+        self.fraTop.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.fraTop.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.fraTop.setObjectName("fraTop")
+        self.gridLayout = QtWidgets.QGridLayout(self.fraTop)
+        self.gridLayout.setContentsMargins(11, 11, 11, 11)
         self.gridLayout.setSpacing(6)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.tblDemands = QTableWidget(self.fraTop)
+        self.gridLayout.setObjectName("gridLayout")
+        self.tblDemands = QtWidgets.QTableWidget(self.fraTop)
         self.tblDemands.setRowCount(10)
-        self.tblDemands.setObjectName(_fromUtf8("tblDemands"))
+        self.tblDemands.setObjectName("tblDemands")
         self.tblDemands.setColumnCount(3)
-        item = QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tblDemands.setHorizontalHeaderItem(0, item)
-        item = QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tblDemands.setHorizontalHeaderItem(1, item)
-        item = QTableWidgetItem()
+        item = QtWidgets.QTableWidgetItem()
         self.tblDemands.setHorizontalHeaderItem(2, item)
         self.gridLayout.addWidget(self.tblDemands, 0, 0, 1, 1)
         self.verticalLayout.addWidget(self.fraTop)
-        self.fraOKCancel = QFrame(self.centralWidget)
-        self.fraOKCancel.setFrameShape(QFrame.StyledPanel)
-        self.fraOKCancel.setFrameShadow(QFrame.Raised)
-        self.fraOKCancel.setObjectName(_fromUtf8("fraOKCancel"))
-        self.horizontalLayout = QHBoxLayout(self.fraOKCancel)
-        # self.horizontalLayout.setMargin(11)
+        self.fraOKCancel = QtWidgets.QFrame(self.centralWidget)
+        self.fraOKCancel.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.fraOKCancel.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.fraOKCancel.setObjectName("fraOKCancel")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.fraOKCancel)
+        self.horizontalLayout.setContentsMargins(11, 11, 11, 11)
         self.horizontalLayout.setSpacing(6)
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        spacerItem = QSpacerItem(338, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(338, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.cmdOK = QPushButton(self.fraOKCancel)
-        self.cmdOK.setObjectName(_fromUtf8("cmdOK"))
+        self.cmdOK = QtWidgets.QPushButton(self.fraOKCancel)
+        self.cmdOK.setObjectName("cmdOK")
         self.horizontalLayout.addWidget(self.cmdOK)
-        self.cmdCancel = QPushButton(self.fraOKCancel)
-        self.cmdCancel.setObjectName(_fromUtf8("cmdCancel"))
+        self.cmdCancel = QtWidgets.QPushButton(self.fraOKCancel)
+        self.cmdCancel.setObjectName("cmdCancel")
         self.horizontalLayout.addWidget(self.cmdCancel)
         self.verticalLayout.addWidget(self.fraOKCancel)
         frmDemands.setCentralWidget(self.centralWidget)
@@ -79,13 +64,14 @@ class Ui_frmDemands(object):
         QtCore.QMetaObject.connectSlotsByName(frmDemands)
 
     def retranslateUi(self, frmDemands):
-        frmDemands.setWindowTitle(_translate("frmDemands", "EPANET Demands Editor", None))
+        _translate = QtCore.QCoreApplication.translate
+        frmDemands.setWindowTitle(_translate("frmDemands", "EPANET Demands Editor"))
         item = self.tblDemands.horizontalHeaderItem(0)
-        item.setText(_translate("frmDemands", "Base Demand", None))
+        item.setText(_translate("frmDemands", "Base Demand"))
         item = self.tblDemands.horizontalHeaderItem(1)
-        item.setText(_translate("frmDemands", "Time Pattern", None))
+        item.setText(_translate("frmDemands", "Time Pattern"))
         item = self.tblDemands.horizontalHeaderItem(2)
-        item.setText(_translate("frmDemands", "Category", None))
-        self.cmdOK.setText(_translate("frmDemands", "OK", None))
-        self.cmdCancel.setText(_translate("frmDemands", "Cancel", None))
+        item.setText(_translate("frmDemands", "Category"))
+        self.cmdOK.setText(_translate("frmDemands", "OK"))
+        self.cmdCancel.setText(_translate("frmDemands", "Cancel"))
 

95637bec09d1d3c27e127666a4bf897c6d614d82
PaulDudaRESPEC <paul.duda@respec.com>
2018-08-24 16:31:28 -0400

    model_utility.py -- using Qt5Agg with matplotlib, fixes reaction report issue #220

--- src/ui/EPANET/frmCalibrationReport.py
+++ src/ui/EPANET/frmCalibrationReport.py
@@ -2,7 +2,7 @@ import PyQt5.QtGui as QtGui
 import PyQt5.QtCore as QtCore
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QSizePolicy
 from ui.help import HelpHandler
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.font_manager import FontProperties
 # import matplotlib.pyplot as plt

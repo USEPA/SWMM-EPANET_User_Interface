9852b17154571045cc934ee025efde250c7a1442
Tong Zhai <tong.zhai@respec.com>
2020-02-12 11:30:55 -0500

    SWMM aquifer editor gets model objects first during loading, address #371

--- src/ui/SWMM/frmAquifers.py
+++ src/ui/SWMM/frmAquifers.py
@@ -18,10 +18,26 @@ class frmAquifers(frmGenericPropertyEditor):
         frmGenericPropertyEditor.__init__(self, main_form, main_form.project.aquifers, edit_these, new_item,
                                           "SWMM " + self.SECTION_TYPE.__name__ + " Editor")
 
+        self.project_section = main_form.project.aquifers
+        if self.project_section and \
+                isinstance(self.project_section.value, list) and \
+                len(self.project_section.value) > 0 and \
+                isinstance(self.project_section.value[0], self.SECTION_TYPE):
+
+            if edit_these:  # Edit only specified item(s) in section
+                if isinstance(edit_these[0], str):  # Translate list from names to objects
+                    edit_names = edit_these
+                    edit_objects = [item for item in self.project_section.value if item.name in edit_these]
+                    edit_these = edit_objects
+
+            else:  # Edit all items in section
+                edit_these = []
+                edit_these.extend(self.project_section.value)
+
+        self.pattern_section = main_form.project.patterns
+        pattern_list = self.pattern_section.value[0:]
         for column in range(0, self.tblGeneric.columnCount()):
             # for patterns, show available patterns
-            pattern_section = main_form.project.patterns
-            pattern_list = pattern_section.value[0:]
             combobox = QComboBox()
             combobox.addItem('')
             selected_index = 0

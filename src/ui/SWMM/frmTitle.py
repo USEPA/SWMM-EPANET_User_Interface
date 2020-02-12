65b161ffa02752f9b7a865e5b024612a646a56be
barrc <barr.colleen@epa.gov>
2020-02-12 15:39:33 -0800

    Revert initial attempt to save geometry for Title/Notes
    This reverts commit 30e07f65e4075e212b0fe8090da6c45e4742c26a.

--- src/ui/SWMM/frmTitle.py
+++ src/ui/SWMM/frmTitle.py
@@ -13,7 +13,6 @@ class frmTitle(QMainWindow, Ui_frmTitle):
         self.cmdCancel.clicked.connect(self.cmdCancel_Clicked)
         self.set_from(main_form.project)
         self._main_form = main_form
-        self.show_widget_values()
 
     def set_from(self, project):
         self.txtTitle.setPlainText(project.title.title)
@@ -23,19 +22,7 @@ class frmTitle(QMainWindow, Ui_frmTitle):
         if section.title != self.txtTitle.toPlainText():
             self._main_form.mark_project_as_unsaved()
         section.title = self.txtTitle.toPlainText()
-        self.save_widget_values()
         self.close()
 
     def cmdCancel_Clicked(self):
-        self.save_widget_values()
         self.close()
-
-    def save_widget_values(self):
-        qsettings = QtCore.QSettings()
-        qsettings.setValue("geometry", self.saveGeometry())
-        qsettings.setValue("windowState", self.saveState())
-
-    def show_widget_values(self):
-        qsettings = QtCore.QSettings()
-        self.restoreGeometry(qsettings.value("geometry", self.saveGeometry(), type=QtCore.QByteArray))
-        self.restoreState(qsettings.value("windowState", self.windowState(), type=QtCore.QByteArray))

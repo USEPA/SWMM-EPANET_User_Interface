c003eb516599bec3e82b4404c39aa17f76c2cbf4
barrc <barr.colleen@epa.gov>
2020-03-05 16:07:20 -0800

    Save geometry and state for frmControls

--- src/ui/SWMM/frmControls.py
+++ src/ui/SWMM/frmControls.py
@@ -16,6 +16,13 @@ class frmControls(QMainWindow, Ui_frmControls):
         self.set_from(main_form.project)
         self._main_form = main_form
 
+        if (main_form.program_settings.value("Geometry/" + "frmControls_geometry") and
+                main_form.program_settings.value("Geometry/" + "frmControls_state")):
+            self.restoreGeometry(main_form.program_settings.value("Geometry/" + "frmControls_geometry",
+                                                                  self.geometry(), type=QtCore.QByteArray))
+            self.restoreState(main_form.program_settings.value("Geometry/" + "frmControls_state",
+                                                               self.windowState(), type=QtCore.QByteArray))
+
     def set_from(self, project):
         # section = core.epanet.project.Control()
 
@@ -25,7 +32,12 @@ class frmControls(QMainWindow, Ui_frmControls):
         if str(self.txtControls.toPlainText()) != self._main_form.project.controls.value:
             self._main_form.mark_project_as_unsaved()
         self._main_form.project.controls.value = str(self.txtControls.toPlainText())
+
+        self._main_form.program_settings.setValue("Geometry/" + "frmControls_geometry", self.saveGeometry())
+        self._main_form.program_settings.setValue("Geometry/" + "frmControls_state", self.saveState())
         self.close()
 
     def cmdCancel_Clicked(self):
+        self._main_form.program_settings.setValue("Geometry/" + "frmControls_geometry", self.saveGeometry())
+        self._main_form.program_settings.setValue("Geometry/" + "frmControls_state", self.saveState())
         self.close()

c9033e2e9315cb82fc0fcdaae602a46d86035c79
PaulDudaRESPEC <paul.duda@respec.com>
2020-05-28 15:35:42 -0400

    frmMain.py, small fix after resolve issue #353 using hasattr

--- src/ui/frmMain.py
+++ src/ui/frmMain.py
@@ -1910,7 +1910,7 @@ class frmMain(QMainWindow, Ui_frmMain):
             # window.destroyed = lambda s, e, a: self._forms.remove(s)
             # window.connect(window, QtCore.SIGNAL('triggered()'), self.editor_closing)
             window.setWindowModality(QtCore.Qt.ApplicationModal)
-            if window.section:
+            if hasattr(window,'section'):
                 if window.section.SECTION_NAME == "[TRANSECTS]" or window.section.SECTION_NAME == "[CURVES]" or window.section.SECTION_NAME == "[TIMESERIES]":
                     # some windows have to be nonmodal so user can go on and browse plots
                     window.setWindowModality(QtCore.Qt.NonModal)
